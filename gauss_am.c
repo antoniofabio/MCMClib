782cc430e74da8d6420395d517c9064d489c3250
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-07-30 16:16:17 +0200

    moved all AM extra data in a separated structure

--- gauss_am.c
+++ gauss_am.c
@@ -1,20 +1,50 @@
-#include "common.h"
+#include "gauss_am.h"
+
+mcmclib_gauss_am_data* mcmclib_gauss_am_alloc(const gsl_matrix* sigma_zero, int t0) {
+	mcmclib_gauss_am_data* ans = (mcmclib_gauss_am_data*) malloc(sizeof(mcmclib_gauss_am_data));
+	int d = sigma_zero->size1;
+	ans->sigma_zero = gsl_matrix_alloc(d, d);
+	ans->t0 = t0;
+	ans->mean = gsl_vector_alloc(d);
+	ans->cov = gsl_matrix_alloc(d, d);
+	ans->t = 0;
+	ans->old = gsl_vector_alloc(d);
+
+	gsl_vector_set_zero(ans->mean);
+	gsl_matrix_set_identity(ans->cov);
+	return ans;
+}
+
+void mcmclib_gauss_am_free(mcmclib_gauss_am_data* p) {
+	gsl_matrix_free(p->sigma_zero);
+	gsl_vector_free(p->mean);
+	gsl_matrix_free(p->cov);
+	gsl_vector_free(p->old);
+	free(p);
+}
+
 
 int mcmclib_gauss_am(const gsl_rng* r,
 	double (*loglik) (gsl_vector* x, const void* data), gsl_vector* x, const void* data,
-	gsl_matrix* sigma_zero, int t0,
-	gsl_matrix* cov, gsl_vector* mean, int* t) {
-
-	//FIXME
+	mcmclib_gauss_am_data* e) {
 
 	int d = x->size;
-	gsl_vector* old = gsl_vector_alloc(d);
+	gsl_vector* old = e->old;
+	gsl_matrix* sigma_zero = e->sigma_zero;
+	gsl_matrix* cov = e->cov;
+	gsl_vector* mean = e->mean;
+	int t0 = e->t0;
+	int *t = &(e->t);
+
+	(*t)++;
 	gsl_vector_memcpy(old, x);
 	double loglik_old, loglik_new, lik_ratio;
 
 	loglik_old = loglik(x, data);
 
-	//TODO: multivariate normal distribution sampler
+	/*sample a new value for 'x', with mean 'old' and assigned covariance matrix*/
+	mcmclib_mvnorm(r, (*t) < t0 ? sigma_zero : cov, x);
+	gsl_vector_add(x, old);
 
 	if(!isfinite(loglik_old)) {
 		gsl_vector_free(old);
--- gauss_am.h
+++ gauss_am.h
@@ -7,21 +7,37 @@
 #include <gsl/gsl_randist.h>
 
 #include "common.h"
+#include "mvnorm.h"
+
+/** Adaptive Metropolis Gaussian random walk extra data
+*/
+typedef struct {
+	gsl_matrix* sigma_zero;
+	int t0;
+	gsl_vector* mean;
+	gsl_matrix* cov;
+	int t;
+	gsl_vector* old;
+} mcmclib_gauss_am_data;
+
+/** alloc (and init) extra AM data
+@param sigma_zero starting proposal covariance matrix
+@param t0 burn-in length before starting adaptation
+*/
+mcmclib_gauss_am_data* mcmclib_gauss_am_alloc(const gsl_matrix* sigma_zero, int t0);
+/** free extra AM data
+*/
+void mcmclib_gauss_am_free(mcmclib_gauss_am_data* p);
 
 /** Adaptive Metropolis Gaussian random walk
 @param r RNG state
 @param loglik pointer to a log-likelihood function
 @param x current point value
 @param data extra data to be passed to the log-likelihood function
-@param sigma_zero starting proposal covariance matrix
-@param t0 burn-in length before starting adaptation
-@param cov pointer to sample covariance matrix observed so far
-@param mean pointer to sample mean observed so far
-@param t pointer to the number of iterations performed so far
+@param extra pointer to extra AM data
 */
 int mcmclib_gauss_am(const gsl_rng* r,
 	double (*loglik) (gsl_vector* x, const void* data), gsl_vector* x, const void* data,
-	gsl_matrix* sigma_zero, int t0,
-	gsl_matrix* cov, int* t);
+	mcmclib_gauss_am_data*);
 
 #endif
