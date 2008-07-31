5930b9ce6207433999ada8507d92604d8c659355
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-07-31 18:34:13 +0200

    added gsl discrete univariate distributions

---
+++ src/lpdf_gsl_discr.h
@@ -0,0 +1,31 @@
+#ifndef __LPDF_GSL_CONT_H__
+#define __LPDF_GSL_CONT_H__
+
+/* INTERNAL UTILITY MACROS*/
+#define TYPE_PAR(prefix) prefix ## _lpdf_p
+
+#define DECLARE_2PAR(prefix, type1, par1, type2, par2) \
+typedef struct {\
+	type1 * par1;\
+	type2 * par2;\
+} TYPE_PAR(prefix);\
+TYPE_PAR(prefix)* mcmclib_ ## prefix ## _lpdf_alloc(type1 * par1, type2 * par2);\
+void mcmclib_ ## prefix ## _lpdf_free(TYPE_PAR(prefix)* p);
+
+#define DECLARE_1PAR(prefix, type1, par1) \
+typedef struct {\
+	type1 * par1;\
+} TYPE_PAR(prefix);\
+TYPE_PAR(prefix)* mcmclib_ ## prefix ## _lpdf_alloc(type1 * par1);\
+void mcmclib_ ## prefix ## _lpdf_free(TYPE_PAR(prefix)* p);
+/*END OF INTERNAL UTILITY MACROS*/
+
+DECLARE_1PAR(poisson, double, mu)
+DECLARE_1PAR(bernoulli, double, p)
+DECLARE_2PAR(binomial, double, p, int, n)
+DECLARE_2PAR(negative_binomial, double, p, int, n)
+DECLARE_2PAR(pascal, double, p, int, n)
+DECLARE_1PAR(geometric, double, p)
+DECLARE_1PAR(logarithmic, double, p)
+
+#endif
