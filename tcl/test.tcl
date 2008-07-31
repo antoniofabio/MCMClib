95f2e3fff3a425d86f0a0d6a602e2fc9ed228151
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-07-31 11:17:10 +0200

    shortened some tcl proc names

--- tcl/test.tcl
+++ tcl/test.tcl
@@ -4,7 +4,7 @@ source ./util.tcl
 ##vector_list
 set lst [mcmclib_vector_list_alloc]
 stopifne [vector_list_str_next_get $lst] NULL
-set v [list2vector [list 1 2 3]]
+set v [l2v [list 1 2 3]]
 set last [mcmclib_vector_list_append $v $lst]
 stopifne [vector_list_str_v_get $last] $v
 
@@ -12,14 +12,14 @@ stopifne [vector_list_str_v_get $last] $v
 set r [gsl_rng_alloc $gsl_rng_default]
 
 ##gaussian random walk
-set x [list2vector 1]
+set x [l2v 1]
 mcmclib_gauss_rw $r $mcmclib_test_dunif $x NULL 1.0
 
-set x [list2vector 0.7]
+set x [l2v 0.7]
 set chain [list]
 for {set i 0} {$i < 10000} {incr i} {
 	mcmclib_gauss_rw $r $mcmclib_test_dunif $x NULL 0.1
-	set lx [vector2list $x]
+	set lx [v2l $x]
 	stopifnot [expr ($lx >= 0) & ($lx <= 1)]
 	lappend chain $lx
 }
--- tcl/util_gsl.tcl
+++ tcl/util_gsl.tcl
@@ -1,4 +1,4 @@
-proc vector2list {v} {
+proc v2l {v} {
 	set ans [list]
 	set d [gsl_vector_size_get $v]
 	for {set i 0} {$i < $d} {incr i} {
@@ -7,7 +7,7 @@ proc vector2list {v} {
 	return $ans
 }
 
-proc list2vector {l} {
+proc l2v {l} {
 	set ans [gsl_vector_alloc [llength $l]]
 	for {set i 0} {$i < [llength $l]} {incr i} {
 		gsl_vector_set $ans $i [lindex $l $i]
