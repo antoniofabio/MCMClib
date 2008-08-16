ea70c6a278a2830ad1ba621767359d37c61db683
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-08-16 09:19:17 +0200

    added tcl list<->array conversion functions

--- tcl/util.tcl
+++ tcl/util.tcl
@@ -10,5 +10,21 @@ proc stopifne {a b} {
 	}
 }
 
+#convert a list into a double array, and back
+proc ll2a {ll} {
+	set ans [new_doubleArray [llength $ll]]
+	for {set i 0} {$i < [llength $ll]} {incr i} {
+		doubleArray_setitem $ans $i [lindex $ll $i]
+	}
+	return $ans
+}
+proc a2ll {a} {
+	set ans [list]
+	for {set i 0} {$i < [llength $ll]} {incr i} {
+		lappend ans [doubleArray_getitem $ans $i]
+	}
+	return $ans
+}
+
 source ./util_gsl.tcl
 source ./util_vector.tcl
