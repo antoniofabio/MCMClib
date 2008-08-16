42ffdd866f9e6b8f443a5b8913b7616877051264
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-08-16 14:19:12 +0200

    improved gaussian distrib test

--- tcl/test_distr.tcl
+++ tcl/test_distr.tcl
@@ -3,6 +3,7 @@ source ./util.tcl
 
 set pg [mcmclib_gaussian_lpdf_alloc [l2a 1.0]]
 set x [l2v 0.0]
-mcmclib_gaussian_lpdf $x $pg
+set value [mcmclib_gaussian_lpdf $x $pg]
+stopifne [expr round($value * 1e6)] -918939
 set cb $mcmclib_gaussian_lpdf_cb
 mcmclib_gaussian_lpdf_free $pg
