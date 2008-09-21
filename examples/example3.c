8882ec50188ce791a345b7c61b8640a05bf491c1
Antonio Di Narzo <antonio.fabio@gmail.com>
2008-09-21 16:51:27 -0400

    example3 compiles correctly

--- examples/example3.c
+++ examples/example3.c
@@ -65,16 +65,65 @@ double target_logdensity(void* ignore, gsl_vector* x) {
 }
 
 int main(int argc, char** argv) {
+	/*load and setup input data*/
 	gsl_matrix* data = gsl_matrix_alloc(NR, 2);
 	FILE* fdata = fopen(DATA_FNAME, "r");
 	if(!fdata)
 		return(1);
 	gsl_matrix_fscanf(fdata, data);
-
 	gsl_vector_view cv = gsl_matrix_column(data, 0);
 	xx = &(cv.vector);
 	cv = gsl_matrix_column(data, 1);
 	nn = &(cv.vector);
 
+	int d = DIM;
+	/*set starting guess covariance matrix*/
+	gsl_matrix* Sigma_zero = gsl_matrix_alloc(d, d);
+	gsl_matrix_set_identity(Sigma_zero);
+	gsl_matrix_scale(Sigma_zero, V0);
+
+	/*alloc a new RNG*/
+	gsl_rng *r = gsl_rng_alloc(gsl_rng_default);
+	/*lets go sample from K different chains in parallel*/
+	mcmclib_gauss_inca_pool* pool = mcmclib_gauss_inca_pool_alloc(Sigma_zero, T0, K);
+	gsl_vector* xx[K];
+	mcmclib_gauss_inca* sampler[K];
+	for(int k=0; k<K; k++) {
+		sampler[k] = mcmclib_gauss_inca_alloc(pool);
+		xx[k] = gsl_vector_alloc(d);
+		/*set starting values at random*/
+		for(int j=0; j<d; j++)
+			gsl_vector_set(xx[k], j, gsl_cdf_logistic_Q(gsl_rng_uniform(r), 1.0));
+	}
+
+	/*open output files*/
+	FILE* out[K];
+	char filename[512];
+	for(int i=0; i<K; i++) {
+		sprintf(filename, "chain_%d.csv", i);
+		out[i] = fopen(filename, "w");
+		/*print out csv header*/
+		fprintf(out[i], "eta, pi1, pi2, gamma\n");
+	}
+
+	/*main MCMC loop: for each time step iterate troughout the K chains*/
+	for(int i=0; i<N; i++) for(int k=0; k<K; k++) {
+		mcmclib_gauss_inca_update(sampler[k], r, target_logdensity, xx[k], NULL);
+		for(int j=0; j<(d-1); j++)
+			fprintf(out[k], "%f, ", gsl_vector_get(xx[k], j));
+		fprintf(out[k], "%f\n", gsl_vector_get(xx[k], d-1));
+	}
+
+	/*release system resources*/
+	for(int k=0; k<K; k++) {
+		fclose(out[k]);
+		mcmclib_gauss_inca_free(sampler[k]);
+		gsl_vector_free(xx[k]);
+	}
+	mcmclib_gauss_inca_pool_free(pool);
+	gsl_rng_free(r);
+	gsl_matrix_free(Sigma_zero);
+
 	fclose(fdata);
+	return 0;
 }
