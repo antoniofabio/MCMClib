243476a2c9c6059d1de2ac3d813a4bdcb9899b91
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-09-21 09:07:50 -0400

    now doing pertinent scaling on gauss_am proposal covariance matrix

--- src/gauss_am.c
+++ src/gauss_am.c
@@ -12,6 +12,8 @@ mcmclib_gauss_am* mcmclib_gauss_am_alloc(const gsl_matrix* sigma_zero, int t0) {
 	ans->t = 0;
 	ans->old = gsl_vector_alloc(d);
 	gsl_matrix_memcpy(ans->sigma_zero, sigma_zero);
+	ans->sf = (2.38 * 2.38) / d;
+	ans->sigma_prop = gsl_matrix_alloc(d, d);
 
 	gsl_vector_set_zero(ans->mean);
 	gsl_matrix_set_zero(ans->cov);
@@ -19,6 +21,7 @@ mcmclib_gauss_am* mcmclib_gauss_am_alloc(const gsl_matrix* sigma_zero, int t0) {
 }
 
 void mcmclib_gauss_am_free(mcmclib_gauss_am* p) {
+	gsl_matrix_free(p->sigma_prop);
 	gsl_matrix_free(p->sigma_zero);
 	gsl_vector_free(p->mean);
 	gsl_matrix_free(p->cov);
@@ -33,19 +36,22 @@ int mcmclib_gauss_am_update(mcmclib_gauss_am* e, const gsl_rng* r,
 	gsl_vector* old = e->old;
 	gsl_matrix* sigma_zero = e->sigma_zero;
 	gsl_matrix* cov = e->cov;
+	gsl_matrix* sigma_prop =  e->sigma_prop;
 	gsl_vector* mean = e->mean;
 	int t0 = e->t0;
 	int *t = &(e->t);
 
 	gsl_vector_memcpy(old, x);
 	/*sample a new value for 'x', with mean 'old' and assigned covariance matrix*/
-	mcmclib_mvnorm(r, ((*t)+1) < t0 ? sigma_zero : cov, x);
+	mcmclib_mvnorm(r, ((*t)+1) < t0 ? sigma_zero : sigma_prop, x);
 	gsl_vector_add(x, old);
 
 	mcmclib_metropolis_symmetric_step(r, old, x, logdistr, data);
 
 	/*adapt extra parameters*/
 	mcmclib_covariance_update(cov, mean, t, x);
+	gsl_matrix_memcpy(sigma_prop, cov);
+	gsl_matrix_scale(sigma_prop, e->sf);
 
 	return 0;
 }
