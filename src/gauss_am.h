b4b03612f22fe97daec41758ed52348d506b6ce2
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-10-01 12:38:27 -0400

    refactored gauss_am sampler: now a simpler itfc

--- src/gauss_am.h
+++ src/gauss_am.h
@@ -12,33 +12,40 @@
 /** Adaptive Metropolis Gaussian random walk extra data
 */
 typedef struct {
+	/**common MCMC fields*/
+	gsl_rng* r;
+	distrfun_p logdistr;
+	void* logdistr_data;
+	gsl_vector* current_x;
+	gsl_vector* old;
+	/**AM specific fields*/
 	gsl_matrix* sigma_zero;
 	int t0;
 	gsl_vector* mean;
 	gsl_matrix* cov;
 	int t;
-	gsl_vector* old;
 	double sf;
 	gsl_matrix* sigma_prop;
 } mcmclib_gauss_am;
 
 /** alloc (and init) extra AM data
+@param r RNG state
+@param logdistr pointer to a log-distribution function
+@param logdistrib_data extra data to be passed to the log-distribution function
+@param start_x starting value
 @param sigma_zero starting proposal covariance matrix
 @param t0 burn-in length before starting adaptation
 */
-mcmclib_gauss_am* mcmclib_gauss_am_alloc(const gsl_matrix* sigma_zero, int t0);
+mcmclib_gauss_am* mcmclib_gauss_am_alloc(gsl_rng* r,
+	distrfun_p logdistr, void* logdistr_data, gsl_vector* start_x,
+	const gsl_matrix* sigma_zero, int t0);
 /** free extra AM data
 */
 void mcmclib_gauss_am_free(mcmclib_gauss_am* p);
 
 /** Adaptive Metropolis Gaussian random walk
-@param extra pointer to extra AM data
-@param r RNG state
-@param logdistr pointer to a log-distribution function
-@param x current point value
-@param data extra data to be passed to the log-distribution function
+@param p pointer to an AM object
 */
-int mcmclib_gauss_am_update(mcmclib_gauss_am* extra, const gsl_rng* r,
-	distrfun_p logdistr, gsl_vector* x, void* data);
+int mcmclib_gauss_am_update(mcmclib_gauss_am* p);
 
 #endif
