64ec4f214de6bd511613d72025d90b3e3cf0f021
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-10-30 19:17:52 -0400

    added generic metropolis step function

--- src/metropolis.h
+++ src/metropolis.h
@@ -2,6 +2,11 @@
 #define __METROPOLIS_H__
 #include "common.h"
 
+/*data: extra data
+  x: conditioning value
+  y: new value*/
+typedef double (*proposal_distr_t) (void* data, gsl_vector* x, gsl_vector* y);
+
 /** Some common M-H support functions */
 
 /**returns 1 for accept, 0 for reject
@@ -12,5 +17,18 @@
 @param data extra data for 'logdistr'
 */
 int mcmclib_metropolis_symmetric_step(const gsl_rng* r, gsl_vector* old, gsl_vector* x, distrfun_p logdistr, void* data);
+
+/**returns 1 for accept, 0 for reject
+@param r GSL RNG
+@param old old value
+@param x vector holding current value (will be eventually updated!)
+@param logdistr ptr to log-distribution function
+@param data extra data for 'logdistr'
+@param q proposal log-distribution
+@param q_data extra data for 'q'
+*/
+int mcmclib_metropolis_generic_step(const gsl_rng* r, gsl_vector* old,
+				    gsl_vector* x, distrfun_p logdistr, void* data,
+				    proposal_distr_t q, void* q_data);
 
 #endif
