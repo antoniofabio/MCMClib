4b92363fb5569ebd7abb774f86c944032fb034c2
Antonio Di Narzo <antonio.fabio@gmail.com>
2008-10-31 10:02:36 -0400

    updated examples compile script

--- examples/compile.sh
+++ examples/compile.sh
@@ -2,4 +2,5 @@ gcc --std=gnu99 -O0 -g $CFLAGS example1.c ../src/libmcmclib.a -I../src $LDFLAGS 
 gcc --std=gnu99 -O0 -g $CFLAGS example2.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o example2
 gcc --std=gnu99 -O0 -g $CFLAGS example3.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o example3
 
+gcc --std=gnu99 -O0 -g $CFLAGS exRapt.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o exRapt
 
