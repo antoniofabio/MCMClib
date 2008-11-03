dee9791267068345f22d063c997f6fa631d30e67
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-11-03 10:51:04 -0500

    moved rapt examples in separate subfolder

--- examples/compile.sh
+++ examples/compile.sh
@@ -2,6 +2,3 @@ gcc --std=gnu99 -O0 -g $CFLAGS example1.c ../src/libmcmclib.a -I../src $LDFLAGS 
 gcc --std=gnu99 -O0 -g $CFLAGS example2.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o example2
 gcc --std=gnu99 -O0 -g $CFLAGS example3.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o example3
 
-gcc --std=gnu99 -O0 -g $CFLAGS exRapt.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o exRapt
-gcc --std=gnu99 -O0 -g $CFLAGS exRapt2.c ../src/libmcmclib.a -I../src $LDFLAGS -lgsl -lgslcblas -lm -o exRapt2
-
