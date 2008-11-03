3f18fcd52e4b2518d6c6e672e66f136899e84acd
Antonio Di Narzo <antonio.fabio@gmail.com>
2008-11-02 20:33:43 -0500

    largely extended rapt2 example R an. script

--- examples/exRapt2.R
+++ examples/exRapt2.R
@@ -9,3 +9,51 @@ nu <- nrow(chu)
 plot(chu[1e4 + 1:100,], type="l")
 ch2 <- chu[-seq_len(nu/2),]
 plot(ch2, type="p", pch=16, cex=0.2)
+
+#load local performance infos
+w <- read.csv("out_rapt_extra.csv")
+w <- subset(w, proposal != 2)
+wx <- w[,1:2]
+wp <- w$proposal
+wn0 <- 1-rank(w$ntries0[wp==0])/sum(wp==0)
+wn1 <- 1-rank(w$ntries1[wp==1])/sum(wp==1)
+plot(wn0 ~ wx[wp==0,1], type="n")
+lines(lowess(wx[wp==0,1], wn0))
+
+w$tag <- apply(w[,c("ntries0", "ntries1")], 1, which.min)
+table(w$tag)
+plot(x1~x0, col=tag, pch=16, cex=0.2, data=w)
+library(MASS)
+m.lda <- lda(factor(tag) ~ x0 + x1, data=w)
+library(nnet)
+m.nnet <- nnet(factor(tag) ~ x0 + x1, data=w, size=2)
+set.seed(1234)
+x0 <- w[sample(nrow(w), size=5000),c("x0", "x1")]
+tag.hat <- as.numeric(predict(m.nnet, x0, type="class"))
+plot(x0[,1], x0[,2], col=tag.hat, pch=16, cex=0.2)
+plot(x0[,1], x0[,2], col=predict(m.lda, x0)$class, pch=16, cex=0.2)
+
+library(lattice)
+xyplot(ntries0~x0 | factor(proposal), data=w)
+
+nrow(w)
+w <- subset(w, is.finite(w))
+nrow(w)
+#library(nnet)
+#m0 <- nnet(w ~ x0 + x1, data=w, subset= proposal==0, size=2, linout=TRUE)
+#m1 <- nnet(w ~ x0 + x1, data=w, subset= proposal==1, size=2, linout=TRUE)
+library(mgcv)
+m0 <- gam(w ~ s(x0) + s(x1), data=w, subset= proposal==0)
+m1 <- gam(w ~ s(x0) + s(x1), data=w, subset= proposal==1)
+
+y0 <- matrix(, 5000, 2)
+y0[, 1] <- predict(m0, newdata=x0)
+y0[, 2] <- predict(m1, newdata=x0)
+summary(y0)
+tags <- apply(y0, 1, which.max)
+table(tags)
+plot(x0[,1], y0[,1], col=tags, pch=16, cex=0.2)
+plot(x0[,2], y0[,1], col=tags, pch=16, cex=0.2)
+plot(x0[,1], y0[,2], col=tags, pch=16, cex=0.2)
+plot(x0[,2], y0[,2], col=tags, pch=16, cex=0.2)
+
