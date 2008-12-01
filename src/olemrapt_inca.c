9310f3316526f11df3fbb3edf1d12600ab039d7d
Antonio Di Narzo <antonio.fabio@gmail.com>
2008-11-30 20:18:16 -0500

    completed olemrapt-inca (untested)

--- src/olemrapt_inca.c
+++ src/olemrapt_inca.c
@@ -12,11 +12,10 @@ MOI* mcmclib_olemrapt_inca_alloc(gsl_rng* r,
   MOI* a = (MOI*) malloc(sizeof(MOI));
   a->M = M;
   a->ss = (mcmclib_olemrapt**) malloc(M * sizeof(mcmclib_olemrapt*));
-  for(int m=0; m<M; m++) {
+  for(int m=0; m<M; m++)
     a->ss[m] = mcmclib_olemrapt_alloc(r, logdistr, logdistr_data,
 				      x[m], t0, Sigma_zero,
 				      beta_hat, mu_hat, Sigma_hat);
-  }
   a->s = mcmclib_mixolem_suff_alloc(beta_hat->size, mu_hat[0]->size);
   a->workspace = mcmclib_mixolem_suff_alloc(beta_hat->size, mu_hat[0]->size);
   a->gamma_hat = mcmclib_mixolem_suff_alloc(beta_hat->size, mu_hat[0]->size);
@@ -34,14 +33,24 @@ void mcmclib_olemrapt_inca_free(MOI* p){
 }
 
 int mcmclib_olemrapt_inca_update(mcmclib_olemrapt_inca* p) {
-  for(int m=0; m < p->M; m++)
+  int M = p->M;
+  for(int m=0; m < M; m++) {
     mcmclib_olemrapt_update(p->ss[m]);
+    mcmclib_mixolem_suff_memcpy(p->ss[(m+1) % M]->em->s, p->ss[m]->em->s);
+    mcmclib_mixolem_suff_memcpy(p->ss[(m+1) % M]->em->gamma, p->ss[m]->em->gamma);
+  }
   return 0;
 }
 
-/**\FIXME*/
 void mcmclib_olemrapt_inca_update_proposals(mcmclib_olemrapt_inca* p){
-  for(int m=0; m < p->M; m++) {
+  int M = p->M;
+
+  /*update chains proposals*/
+  for(int m=0; m < M; m++) {
     mcmclib_olemrapt_update_proposals(p->ss[m]);
+    if(m < (M-1)) {
+      mcmclib_mixolem_suff_memcpy(p->ss[m+1]->em->s, p->ss[m]->em->s);
+      mcmclib_mixolem_suff_memcpy(p->ss[m+1]->em->gamma, p->ss[m]->em->gamma);
+    }
   }
 }
