dd116b32adcdb76600d36c84722a6adf33c394c3
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-02-15 17:23:20 -0500

    added draft amh implementation (untested)

---
+++ src/amh.c
@@ -0,0 +1,20 @@
+#include "amh.h"
+
+mcmclib_amh* mcmclib_amh_alloc(mcmclib_mh* mh,
+			       mcmclib_amh_update_gamma_p update_gamma) {
+  mcmclib_amh* p = (mcmclib_amh*) malloc(sizeof(mcmclib_amh));
+  p->update_gamma = update_gamma;
+  p->n = 0;
+  return p;
+}
+
+void mcmclib_amh_free(mcmclib_amh* p) {
+  free(p);
+}
+
+int mcmclib_amh_update(mcmclib_amh* p) {
+  mcmclib_mh_update(p->mh);
+  p->n++;
+  p->update_gamma(p);
+  return(p->mh->last_accepted);
+}
---
+++ src/amh.h
@@ -0,0 +1,32 @@
+#ifndef __AMH_H__
+#define __AMH_H__
+
+/**\addtogroup adaptive
+@{
+\defgroup Adatpive Metropolis-Hastings sampling
+*/
+
+#include "mh.h"
+
+typedef void (*mcmclib_amh_update_gamma_p) (void* p);
+
+/**\brief Generic Adaptive Metropolis-Hastings sampler */
+typedef struct {
+  mcmclib_mh* mh;
+  mcmclib_amh_update_gamma_p update_gamma;
+  int n; /**< current iteration number*/
+} mcmclib_amh;
+
+mcmclib_amh* mcmclib_amh_alloc(mcmclib_mh* mh,
+			       mcmclib_amh_update_gamma_p update_gamma);
+
+void mcmclib_amh_free(mcmclib_amh* p);
+
+/**\brief update chain value
+\return 1 if move accepted, 0 if rejected*/
+int mcmclib_amh_update(mcmclib_amh* p);
+
+/**@}*/
+/**@}*/
+
+#endif
