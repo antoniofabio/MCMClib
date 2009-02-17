f841b3a9a37ddf103b3d38ab5ab3102db99cf05c
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-02-17 12:10:16 -0500

    fixed bugs in amh

--- src/amh.c
+++ src/amh.c
@@ -4,6 +4,7 @@ mcmclib_amh* mcmclib_amh_alloc(mcmclib_mh* mh,
 			       mcmclib_amh_update_gamma_p update_gamma,
 			       void* update_gamma_data) {
   mcmclib_amh* p = (mcmclib_amh*) malloc(sizeof(mcmclib_amh));
+  p->mh = mh;
   p->update_gamma = update_gamma;
   p->update_gamma_data = update_gamma_data;
   p->n = 0;
@@ -17,6 +18,6 @@ void mcmclib_amh_free(mcmclib_amh* p) {
 int mcmclib_amh_update(mcmclib_amh* p) {
   mcmclib_mh_update(p->mh);
   p->n++;
-  p->update_gamma(p, p->mh->x);
+  p->update_gamma(p->update_gamma_data, p->mh->x);
   return(p->mh->last_accepted);
 }
