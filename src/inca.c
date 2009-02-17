752c11974a0b2cfdf7c7b6c17b3f0463935f760f
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-02-17 11:18:48 -0500

    better inca implementation

--- src/inca.c
+++ src/inca.c
@@ -8,13 +8,18 @@ mcmclib_inca* mcmclib_inca_alloc(gsl_rng* r,
 				 void* update_gamma_data,
 				 gsl_vector** x, int M) {
   mcmclib_inca* p = (mcmclib_inca*) malloc(sizeof(mcmclib_inca));
-  mcmclib_mh* mh = mcmclib_mh_alloc(r, logdistr, logdistr_data, x[0], qd_fun, qd_data,
+  mcmclib_mh* mh = mcmclib_mh_alloc(r, logdistr, logdistr_data,
+				    gsl_vector_alloc(x[0]->size),
+				    qd_fun, qd_data,
 				    q_sampler, q_sampler_data);
   p->amh = mcmclib_amh_alloc(mh, update_gamma, update_gamma_data);
+  p->x = x;
+  p->M = M;
   return p;
 }
 
 void mcmclib_inca_free(mcmclib_inca* p) {
+  gsl_vector_free(p->amh->mh->x);
   mcmclib_mh_free(p->amh->mh);
   mcmclib_amh_free(p->amh);
   free(p);
@@ -25,13 +30,9 @@ int mcmclib_inca_update(mcmclib_inca* p) {
   mcmclib_mh* mh = amh->mh;
   gsl_vector** x = p->x;
   for(int m=0; m < p->M; m++) {
-    gsl_vector_memcpy(mh->x_old, x[m]);
-    mh->q_sampler(mh->q_sampler_data, x[m]);
-    mh->last_accepted = mcmclib_metropolis_generic_step(mh->r, mh->x_old, x[m],
-							mh->logdistr, mh->logdistr_data,
-							mh->qd_fun, mh->qd_data);
-    amh->n++;
-    amh->update_gamma(amh->update_gamma_data, x[m]);
+    gsl_vector_memcpy(mh->x, x[m]);
+    mcmclib_amh_update(amh);
+    gsl_vector_memcpy(x[m], mh->x);
   }
   return(mh->last_accepted);
 }
