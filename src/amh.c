9b9f874b3e0bfc42d54710e2542ca23f52914390
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-02-27 10:23:48 -0500

    refactored amh object

--- src/amh.c
+++ src/amh.c
@@ -1,12 +1,12 @@
 #include "amh.h"
 
 mcmclib_amh* mcmclib_amh_alloc(mcmclib_mh* mh,
-			       mcmclib_amh_update_gamma_p update_gamma,
-			       void* update_gamma_data) {
+			       void* suff_data,
+			       mcmclib_amh_update_gamma_p update_gamma) {
   mcmclib_amh* p = (mcmclib_amh*) malloc(sizeof(mcmclib_amh));
   p->mh = mh;
+  p->suff_data = suff_data;
   p->update_gamma = update_gamma;
-  p->update_gamma_data = update_gamma_data;
   p->n = 0;
   return p;
 }
@@ -18,6 +18,6 @@ void mcmclib_amh_free(mcmclib_amh* p) {
 int mcmclib_amh_update(mcmclib_amh* p) {
   mcmclib_mh_update(p->mh);
   p->n++;
-  p->update_gamma(p->update_gamma_data, p->mh->x);
+  p->update_gamma(p, p->mh->x);
   return(p->mh->last_accepted);
 }
--- src/amh.h
+++ src/amh.h
@@ -13,14 +13,14 @@ typedef void (*mcmclib_amh_update_gamma_p) (void* p, gsl_vector* x);
 /**\brief Generic Adaptive Metropolis-Hastings sampler */
 typedef struct {
   mcmclib_mh* mh;
+  void* suff_data; /**< sufficient data accumulated up to current iteration*/
   mcmclib_amh_update_gamma_p update_gamma;
-  void* update_gamma_data;
   int n; /**< current iteration number*/
 } mcmclib_amh;
 
 mcmclib_amh* mcmclib_amh_alloc(mcmclib_mh* mh,
-			       mcmclib_amh_update_gamma_p update_gamma,
-			       void* update_gamma_data);
+			       void* suff_data,
+			       mcmclib_amh_update_gamma_p update_gamma);
 
 void mcmclib_amh_free(mcmclib_amh* p);
 
