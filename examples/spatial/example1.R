e01de868e4e3c606f72e3e82c08964716eeb506c
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-03-29 16:02:37 +0200

    added spatial example companion R script

---
+++ examples/spatial/example1.R
@@ -0,0 +1,40 @@
+N <- 9
+MU <- 1.5
+RHO <- 2.0
+SIGMA2 <- 2.0
+TAUSQ <- 0.5
+
+xy <- matrix(, N, 2)
+k <- 1
+for(i in 0:2) for(j in 0:2) {
+  xy[k, 1] <- i
+  xy[k, 2] <- j
+  k <- k + 1
+}
+D <- as.matrix(dist(xy))
+
+library(mvtnorm)
+getSigma <- function(rho, sigma, tausq, D) {
+  N <- nrow(D)
+  Sigma <- diag(sigma, N)
+  for(i in 1:(N-1)) for(j in (i+1):N) {
+    dij <- D[i,j]
+    cij <- (SIGMA2 - TAUSQ) * ( 1 - exp(- dij / RHO) )
+    if(dij > 0)
+      cij <- cij + TAUSQ
+    Sigma[i,j] <- Sigma[j,i] <- SIGMA2 - cij
+  }
+  return(Sigma)
+}
+Sigma <- getSigma(RHO, SIGMA2, TAUSQ, D)
+
+set.seed(1234)
+Y <- rmvnorm(1, rep(0, N), Sigma)
+
+##read chain output
+X <- read.csv("data.csv")
+library(coda)
+xx <- mcmc(X)
+yy <- mcmc(xx[seq(1, 10000, by=10),])
+plot(yy)
+
