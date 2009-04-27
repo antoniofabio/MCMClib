a360b52a6490a820e75442fa185b724b75008dac
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-04-27 10:28:00 +0200

    improved tscam

--- test/tscam.c
+++ test/tscam.c
@@ -7,9 +7,9 @@
 #include <gsl/gsl_matrix.h>
 #include <scam.h>
 
-#define N 1000
+#define N 5
 #define DIM 2
-#define T0 100
+#define T0 2
 
 #define v0(x) gsl_vector_get(x, 0)
 #define v1(x) gsl_vector_get(x, 1)
@@ -17,11 +17,15 @@
 #define x1 v1(x)
 #define m00(m) gsl_matrix_get(m, 0, 0)
 
+static int counter1 = 0;
+
 static double dunif_j(void* ignore, int j, double xj) {
-	double min = j ? 0.0 : 1.0;
-	double max = j ? 1.0 : 2.0;
-  if((xj >= min) && (xj <= max))
+	counter1 ++;
+	double min = j ? 1.0 : 0.0;
+	double max = j ? 2.0 : 1.0;
+  if((xj >= min) && (xj <= max)) {
     return log(1.0);
+	}
   return log(0.0);
 }
 
@@ -33,13 +37,15 @@ int main(int argc, char** argv) {
 	gsl_vector_set(x, 1, 1.0 + gsl_rng_uniform(rng));
 
   mcmclib_scam* s = mcmclib_scam_alloc(rng, dunif_j, NULL,
-																			 x, 1.0, T0);
+																			 x, 0.1, T0);
 
   /*Main MCMC loop*/
   for(int n=0; n<N; n++)
     mcmclib_scam_update(s);
 
   /*check results*/
+	assert(counter1 == (N * 2 * DIM)); /*check that logdistr. has been called the
+																			 right number of times*/
 	assert((gsl_vector_get(x, 0) >= 0.0) && (gsl_vector_get(x, 0) <= 1.0));
 	assert((gsl_vector_get(x, 1) >= 1.0) && (gsl_vector_get(x, 1) <= 2.0));
 
