f5d08445dad6d4d241a2c15fa52a02bc7636f103
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-04-30 18:39:10 +0200

    started writing MCAR model testing prog

---
+++ test/tmcar_model.c
@@ -0,0 +1,73 @@
+/**Test MCAR model */
+#include <stdio.h>
+#include <assert.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <mcar_model.h>
+
+#define TOL 1e-6
+int check_dequal(double a, double b) {
+  return (fabs(a-b) < TOL);
+}
+
+void matrix_printf(gsl_matrix* m) {
+  int p = m->size1;
+  int q = m->size2;
+  for(int i=0; i<p; i++) {
+    for(int j=0; j<q; j++)
+      printf("\t%f", gsl_matrix_get(m, i, j));
+    printf("\n");
+  }
+  printf("\n");
+  fflush(stdout);
+}
+
+#define P 3
+#define N 4
+
+mcmclib_mcar_model* p;
+
+/* declare as near regions 'i' and 'j' */
+#define DECL_AD(i, j) if(1) {			\
+    gsl_matrix_set(W, i, j, 1.0);		\
+    gsl_matrix_set(W, j, i, 1.0);		\
+  }
+
+int main(int argc, char** argv) {
+  gsl_matrix* W = gsl_matrix_alloc(N, N);
+  gsl_matrix_set_zero(W);
+  for(int i=0; i<(N-1); i++)
+    DECL_AD(i, i+1);
+
+  mcmclib_mcar_tilde_lpdf* llik = mcmclib_mcar_tilde_lpdf_alloc(P, N, W);
+  gsl_vector* e = gsl_vector_alloc(N*P);
+  gsl_vector_set_all(e, 2.0);
+  p = mcmclib_mcar_model_alloc(llik, e);
+
+  gsl_vector* alpha_h = gsl_vector_alloc(P * (P-1) / 2);
+  /*  gsl_vector_set_all(alpha_h, 1.0);
+  printf("%f -> %f\n", 1.0, mcmclib_mcar_model_alpha1_lpdf(p, alpha_h));
+  gsl_vector_set_all(alpha_h, 0.5);
+  printf("%f -> %f\n", 0.5, mcmclib_mcar_model_alpha1_lpdf(p, alpha_h));
+  gsl_vector_set_all(alpha_h, -1.0);
+  printf("%f -> %f\n", -1.0, mcmclib_mcar_model_alpha1_lpdf(p, alpha_h));
+  gsl_vector_set_all(alpha_h, 1.0);
+  printf("%f -> %f\n", 1.0, mcmclib_mcar_model_alpha1_lpdf(p, alpha_h));*/
+
+  gsl_vector_set(alpha_h, 0, -M_PI/2.0);
+  assert(mcmclib_mcar_model_alpha1_lpdf(p, alpha_h) == log(0.0));
+  assert(mcmclib_mcar_model_alpha2_lpdf(p, alpha_h) == log(0.0));
+  gsl_vector_set(alpha_h, 0, M_PI/2.0);
+  assert(mcmclib_mcar_model_alpha1_lpdf(p, alpha_h) == log(0.0));
+  assert(mcmclib_mcar_model_alpha2_lpdf(p, alpha_h) == log(0.0));
+  gsl_vector_set(alpha_h, 1, -2.0);
+  assert(mcmclib_mcar_model_alpha1_lpdf(p, alpha_h) == log(0.0));
+  assert(mcmclib_mcar_model_alpha2_lpdf(p, alpha_h) == log(0.0));
+  gsl_vector_free(alpha_h);
+
+  mcmclib_mcar_model_free(p);
+  mcmclib_mcar_tilde_lpdf_free(llik);
+  gsl_matrix_free(W);
+}
