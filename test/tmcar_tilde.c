2dcf8291ff9b7b4fbfc3f266dcc5dd825d469d5d
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-04-30 16:44:49 +0200

    improved mcar_tilde test

--- test/tmcar_tilde.c
+++ test/tmcar_tilde.c
@@ -21,6 +21,7 @@ void matrix_printf(gsl_matrix* m) {
     printf("\n");
   }
   printf("\n");
+  fflush(stdout);
 }
 
 #define P 2
@@ -60,7 +61,7 @@ int main(int argc, char** argv) {
   gsl_vector* mu = gsl_vector_alloc(N * P);
   gsl_vector_set_zero(mu);
   gsl_matrix* W = gsl_matrix_alloc(N, N);
-  gsl_matrix_set_identity(W);
+  gsl_matrix_set_zero(W);
   for(int i=0; i<(N-1); i++)
     DECL_AD(i, i+1);
   
@@ -71,6 +72,19 @@ int main(int argc, char** argv) {
       count += gsl_matrix_get(W, i, j) == 1.0;
     assert(gsl_vector_get(p->m, i) == (double) count);
   }
+
+  for(int i=0; i<(P-1); i++)
+    for(int j=i+1; j<P; j++) {
+      gsl_matrix_set(p->Gamma, i, j, 0.6);
+      gsl_matrix_set(p->Gamma, j, i, 0.6);
+    }
+
+  mcmclib_mcar_tilde_lpdf_update_blocks(p);
+  mcmclib_mcar_tilde_lpdf_update_vcov(p);
+  assert(check_dequal(gsl_matrix_get(p->vcov, 0, 0), 1.363636));
+  assert(check_dequal(gsl_matrix_get(p->vcov, 1, 3), 0.290909));
+  assert(check_dequal(gsl_matrix_get(p->vcov, 4, 1), 0.109091));
+  assert(check_dequal(gsl_matrix_get(p->vcov, 5, 3), 0.145455));
 
   gsl_vector* x = gsl_vector_alloc(N*P);
   gsl_vector_set_all(x, 1.0);
