491e666a125195ec81d4bcad56e3f129c93a66c4
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-01 15:46:47 +0200

    writing Gamma(alpha, sigma) MCAR model lpdf (untested)

--- src/mcar_model.c
+++ src/mcar_model.c
@@ -70,6 +70,51 @@ double mcmclib_mcar_model_sigma_lpdf(mcmclib_mcar_model* p, gsl_vector* sigma) {
   return ans;
 }
 
+/* Givens angles and eigenvalues representation of a pos.def. matrix.
+   Result goes in M */
+void givens_representation(gsl_matrix* M, gsl_vector* alpha, gsl_vector* sigma) {
+  int n = M->size1;
+
+  gsl_matrix* A = gsl_matrix_alloc(n, n);
+  mcmclib_Givens_rotations(A, alpha);
+  gsl_matrix* D = gsl_matrix_alloc(n, n);
+  gsl_matrix_set_zero(D);
+  for(int i=0; i<n; i++)
+    gsl_matrix_set(D, i, i, gsl_vector_get(sigma, i));
+  gsl_matrix* AD = gsl_matrix_alloc(n, n);
+  gsl_matrix_set_zero(AD);
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A, D, 0.0, AD);
+  gsl_matrix_set_zero(M);
+  gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, AD, A, 0.0, M);
+
+  gsl_matrix_free(AD);
+  gsl_matrix_free(D);
+  gsl_matrix_free(A);
+}
+
+double mcmclib_mcar_model_alphasigma_lpdf(mcmclib_mcar_model* p, gsl_vector* alphasigma) {
+  int P = p->lpdf->p;
+  gsl_matrix* Gamma = gsl_matrix_alloc(P, P);
+  gsl_vector_view alpha_v = gsl_vector_subvector(alphasigma, 0, P*(P-1)/2);
+  gsl_vector* alpha = &alpha_v.vector;
+  gsl_vector* alpha1 = gsl_vector_alloc(P*(P-1)/2);
+  for(int i=0; i<P*(P-1)/2; i++) {
+    double bi = exp(gsl_vector_get(alpha, i));
+    gsl_vector_set(alpha1, i, M_PI_2 * (bi - 1.0) / (bi + 1.0));
+  }
+  gsl_vector_view sigma_v = gsl_vector_subvector(alphasigma, P*(P-1)/2, P);
+  gsl_vector* sigma = &sigma_v.vector;
+  gsl_vector* sigma1 = gsl_vector_alloc(P);
+  for(int i=0; i<P; i++)
+    gsl_vector_set(sigma1, i, exp(gsl_vector_get(sigma, i)));
+  givens_representation(Gamma, alpha, sigma1);
+  gsl_vector_free(sigma1);
+  gsl_vector_view gamma_v = gsl_vector_view_array(Gamma->data, P*(P-1)/2);
+  double ans = mcmclib_mcar_model_Gamma_lpdf(p, &gamma_v.vector);
+  gsl_matrix_free(Gamma);
+  return ans;
+}
+
 double mcmclib_mcar_model_Gamma_lpdf(mcmclib_mcar_model* p, gsl_vector* gamma) {
   int P = p->lpdf->p;
   gsl_matrix* tmp = gsl_matrix_alloc(P, P);
