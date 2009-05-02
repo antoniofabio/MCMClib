1f37e10a28db11a2d943d12a63a8c9d53b269ac7
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-02 17:16:19 +0200

    fixed some mistakes in mcar example

--- examples/mcar/example1.c
+++ examples/mcar/example1.c
@@ -4,9 +4,10 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <gauss_am.h>
+#include <mcar_tilde.h>
 #include <mcar_model.h>
 
-#define N 1000
+#define N 1
 #define T0 50
 #define V0 4.0
 
@@ -16,28 +17,26 @@
 
 gsl_vector *alpha1, *alpha2, *sigma, *Gammav;
 gsl_rng* rng;
+mcmclib_mcar_tilde_lpdf* lpdf;
 mcmclib_mcar_model* model;
 mcmclib_amh* sampler[4];
 
 void init_chains() {
   rng = gsl_rng_alloc(gsl_rng_default);
 
-  alpha1 = gsl_vector_alloc(ALPHAP);
-  gsl_vector_set_zero(alpha1);
+  alpha1 = lpdf->alpha1;
   gsl_matrix* Sigma0 = gsl_matrix_alloc(ALPHAP, ALPHAP);
   gsl_matrix_set_identity(Sigma0);
   gsl_matrix_scale(Sigma0, V0 / ALPHAP);
   sampler[0] = mcmclib_gauss_am_alloc(rng, mcmclib_mcar_model_alpha1_lpdf,
 				      model, alpha1, Sigma0, T0);
 
-  alpha2 = gsl_vector_alloc(ALPHAP);
-  gsl_vector_set_zero(alpha2);
+  alpha2 = lpdf->alpha2;
   sampler[1] = mcmclib_gauss_am_alloc(rng, mcmclib_mcar_model_alpha2_lpdf,
 				      model, alpha2, Sigma0, T0);
   gsl_matrix_free(Sigma0);
 
-  sigma = gsl_vector_alloc(P);
-  gsl_vector_set_zero(sigma);
+  sigma = lpdf->sigma;
   Sigma0 = gsl_matrix_alloc(P, P);
   gsl_matrix_set_identity(Sigma0);
   gsl_matrix_scale(Sigma0, V0 / P);
@@ -59,9 +58,6 @@ void free_chains() {
   for(int i=0; i<4; i++)
     mcmclib_gauss_am_free(sampler[i]);
   gsl_rng_free(rng);
-  gsl_vector_free(alpha1);
-  gsl_vector_free(alpha2);
-  gsl_vector_free(sigma);
   gsl_vector_free(Gammav);
 }
 
@@ -75,9 +71,10 @@ int main(int argc, char** argv) {
   } 
   gsl_matrix_fscanf(W_file, W);
   fclose(W_file);
-  mcmclib_mcar_tilde_lpdf* lpdf = mcmclib_mcar_tilde_lpdf_alloc(P, DIM, W);
+  lpdf = mcmclib_mcar_tilde_lpdf_alloc(P, DIM, W);
   gsl_matrix_free(W);
   gsl_vector* e = gsl_vector_alloc(P * DIM);
+  gsl_vector_set_zero(e);
   model = mcmclib_mcar_model_alloc(lpdf, e);
 
   init_chains();
