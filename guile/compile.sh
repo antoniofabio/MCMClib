e9de13e5f1c5969fea18df38cd567726fe152795
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-04 10:39:00 +0200

    fixed guile compile script

--- guile/compile.sh
+++ guile/compile.sh
@@ -1,2 +1,2 @@
 swig -guile mcmclib.i
-gcc -shared --std=gnu99 ${CFLAGS} mcmclib_wrap.c -I../src -lgsl -lgslcblas -lm ${LDFLAGS} ../src/libmcmclib.a -o libschememcmclib.so
+gcc -shared --std=gnu99 ${CFLAGS} -fPIC mcmclib_wrap.c -I../src -lgsl -lgslcblas -lm ${LDFLAGS} ../src/libmcmclib.a -o libschememcmclib.so
