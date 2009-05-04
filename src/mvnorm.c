689543132031fe6ff0674ced776b6af0e3327e65
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-04 14:24:11 +0200

    cleaned mvnorm sources

--- src/mvnorm.c
+++ src/mvnorm.c
@@ -12,46 +12,46 @@
 #include "mvnorm.h"
 
 void mcmclib_mvnorm(const gsl_rng* r,
-	const gsl_matrix* sigma,
-	gsl_vector* out) {
-	int d = sigma->size1;
+		    const gsl_matrix* sigma,
+		    gsl_vector* out) {
+  int d = sigma->size1;
 
-	gsl_matrix* sigma_chol = gsl_matrix_alloc(d, d);
-	gsl_matrix_memcpy(sigma_chol, sigma);
-	gsl_linalg_cholesky_decomp(sigma_chol);
+  gsl_matrix* sigma_chol = gsl_matrix_alloc(d, d);
+  gsl_matrix_memcpy(sigma_chol, sigma);
+  gsl_linalg_cholesky_decomp(sigma_chol);
 
-	mcmclib_mvnorm_chol(r, sigma_chol, out);
-	
-	gsl_matrix_free(sigma_chol);
+  mcmclib_mvnorm_chol(r, sigma_chol, out);
+
+  gsl_matrix_free(sigma_chol);
 }
 
 void mcmclib_mvnorm_chol(const gsl_rng* r,
-	const gsl_matrix* sigma_chol,
-	gsl_vector* out) {
-	/*generate d iid values*/
-	for(int n= (sigma_chol->size1 - 1); n>=0; n--)
-		gsl_vector_set(out, n, gsl_ran_gaussian(r, 1.0));
+			 const gsl_matrix* sigma_chol,
+			 gsl_vector* out) {
+  /*generate d iid values*/
+  for(int n= (sigma_chol->size1 - 1); n>=0; n--)
+    gsl_vector_set(out, n, gsl_ran_gaussian(r, 1.0));
 
-	/*rotate them according to the cholesky 'square root' of sigma*/
-	gsl_blas_dtrmv(CblasLower, CblasNoTrans, CblasNonUnit, sigma_chol, out);
+  /*rotate them according to the cholesky 'square root' of sigma*/
+  gsl_blas_dtrmv(CblasLower, CblasNoTrans, CblasNonUnit, sigma_chol, out);
 }
 
 mcmclib_mvnorm_lpdf* mcmclib_mvnorm_lpdf_alloc(gsl_vector* mean, double* vcov) {
-	int d = mean->size;
-	mcmclib_mvnorm_lpdf* ans = (mcmclib_mvnorm_lpdf*) malloc(sizeof(mcmclib_mvnorm_lpdf));
-	ans->mean = mean;
-	ans->vcov = vcov;
-	ans->rooti = gsl_matrix_alloc(d, d);
-	ans->x_mu = gsl_vector_alloc(d);
-	ans->mahal = gsl_vector_alloc(d);
-	return ans;
+  int d = mean->size;
+  mcmclib_mvnorm_lpdf* ans = (mcmclib_mvnorm_lpdf*) malloc(sizeof(mcmclib_mvnorm_lpdf));
+  ans->mean = mean;
+  ans->vcov = vcov;
+  ans->rooti = gsl_matrix_alloc(d, d);
+  ans->x_mu = gsl_vector_alloc(d);
+  ans->mahal = gsl_vector_alloc(d);
+  return ans;
 }
 
 void mcmclib_mvnorm_lpdf_free(mcmclib_mvnorm_lpdf* p) {
-	gsl_matrix_free(p->rooti);
-	gsl_vector_free(p->x_mu);
-	gsl_vector_free(p->mahal);
-	free(p);
+  gsl_matrix_free(p->rooti);
+  gsl_vector_free(p->x_mu);
+  gsl_vector_free(p->mahal);
+  free(p);
 }
 
 double mcmclib_mvnorm_lpdf_compute(void* in_p, gsl_vector* x) {
