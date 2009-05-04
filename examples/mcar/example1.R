e4974a2ac239e8961cb7c17c2450795cdac804ed
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-04 18:27:19 +0200

    microfixes to mcar example R script

--- examples/mcar/example1.R
+++ examples/mcar/example1.R
@@ -1,5 +1,7 @@
 P <- 3
 as <- matrix(read.table("chain_alpha12sigma.dat")[[1]], ncol=P*P)
-as <- as[seq(1, 5000, by=100),]
+as <- as[seq(1, 5000, by=10),]
 plot(as[,1], type="l")
 plot(as[,P*(P-1)+1], type="l")
+plot(as[,P*(P-1)+2], type="l")
+plot(as[,P*(P-1)+3], type="l")
