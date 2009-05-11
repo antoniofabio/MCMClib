d38b07127276d9b7213fbbab26366d7e4492d5c2
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-11 16:07:41 +0200

    revamped support to hierarchical lpdf building, added testing prog

--- src/lpdf_hierarchical.c
+++ src/lpdf_hierarchical.c
@@ -10,45 +10,47 @@
 #include "lpdf_hierarchical.h"
 
 mcmclib_post_lpdf* mcmclib_post_lpdf_alloc(gsl_vector* x, distrfun_p prior, void* parms,
-	distrfun_p loglik, gsl_vector** childs, void** child_parms) {
-	mcmclib_post_lpdf* ans = (mcmclib_post_lpdf*) malloc(sizeof(mcmclib_post_lpdf));
-	ans->x = x;
-	ans->prior = prior;
-	ans->parms = parms;
-	ans->loglik = loglik;
-	ans->childs = childs;
-	ans->child_parms = child_parms;
-	ans->workspace = gsl_vector_alloc(x->size);
-	return ans;
+					   distrfun_p loglik, gsl_vector** childs,
+					   void** child_parms, int nchilds) {
+  mcmclib_post_lpdf* ans = (mcmclib_post_lpdf*) malloc(sizeof(mcmclib_post_lpdf));
+  ans->x = x;
+  ans->prior = prior;
+  ans->parms = parms;
+  ans->loglik = loglik;
+  ans->childs = childs;
+  ans->child_parms = child_parms;
+  ans->nchilds = nchilds;
+  ans->workspace = gsl_vector_alloc(x->size);
+  return ans;
 }
 
 void mcmclib_post_lpdf_free(mcmclib_post_lpdf* p) {
-	gsl_vector_free(p->workspace);
-	free(p);
+  gsl_vector_free(p->workspace);
+  free(p);
 }
 
 double mcmclib_post_lpdf_compute(void* data, gsl_vector* x) {
-	mcmclib_post_lpdf* d = (mcmclib_post_lpdf*) data;
-	double ans = 0.0;
-	/*store old value*/
-	gsl_vector_memcpy(d->workspace, d->x);
-	/*set new value*/
-	gsl_vector_memcpy(d->x, x);
+  mcmclib_post_lpdf* d = (mcmclib_post_lpdf*) data;
+  double ans = 0.0;
+  /*store old value*/
+  gsl_vector_memcpy(d->workspace, d->x);
+  /*set new value*/
+  gsl_vector_memcpy(d->x, x);
 
-	ans += d->prior(d->parms, d->x);
-	if(!isfinite(ans)) {
-		/*restore old value*/
-		gsl_vector_memcpy(d->x, d->workspace);
-		return(ans);
-	}
-	for(int i=0; d->childs[i] != NULL; i++) {
-		ans += d->loglik(d->child_parms[i], d->childs[i]);
-		if(!isfinite(ans))
-			break;
-	}
+  ans += d->prior(d->parms, d->x);
+  if(!isfinite(ans)) {
+    /*restore old value*/
+    gsl_vector_memcpy(d->x, d->workspace);
+    return(ans);
+  }
+  for(int i=0; d->childs[i] != NULL; i++) {
+    ans += d->loglik(d->child_parms[i], d->childs[i]);
+    if(!isfinite(ans))
+      break;
+  }
 
-	/*restore old value*/
-	gsl_vector_memcpy(d->x, d->workspace);
+  /*restore old value*/
+  gsl_vector_memcpy(d->x, d->workspace);
 
-	return ans;
+  return ans;
 }
