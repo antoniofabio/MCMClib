9922cebeefd938618b8542ac66ffff59065b094b
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-15 11:15:26 +0200

    misc code cleaning

--- src/lm.c
+++ src/lm.c
@@ -8,13 +8,10 @@
  *  (at your option) any later version.
  */
 #include <assert.h>
-#include <math.h>
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_eigen.h>
 #include "matrix.h"
 #include "lm.h"
 
-mcmclib_lm* mcmclib_lm_alloc() {
+mcmclib_lm* mcmclib_lm_alloc(const gsl_matrix* X) {
   mcmclib_lm* ans;
   return ans;
 }
@@ -22,12 +19,11 @@ void mcmclib_lm_free(mcmclib_lm* p) {
   free(p);
 }
 
-/*ported from the MCMCpack R package C++ sources*/
-int NormNormregress_beta_draw(const gsl_matrix* XX, const gsl_vector* XY,
-			      const gsl_vector* b0, const gsl_matrix* B0,
-			      double sigma2, gsl_rng* rng, gsl_vector* out) {
-  // this function gets the cross-product matrix X'X and the matrix X'Y
-  // to minimize the amount of computation within the function
+int mcmclib_lm_beta_draw(gsl_rng* rng, const gsl_matrix* XX, const gsl_vector* XY,
+			 const gsl_vector* b0, const gsl_matrix* B0,
+			 double sigma2, gsl_vector* out) {
+  /* this function gets the cross-product matrix X'X and the matrix X'Y
+     to minimize the amount of computation within the function */
   assert(XX->size1 == XX->size2);
   const unsigned int k = XX->size2;
   const double sig2_inv = 1.0 / sigma2;
@@ -52,26 +48,19 @@ int NormNormregress_beta_draw(const gsl_matrix* XX, const gsl_vector* XY,
   return GSL_SUCCESS;
 }
 
-/*ported from the MCMCpack R package C++ sources*/
-// linear regression with Gaussian errors sigma2 draw 
-// (inverse-Gamma  prior)
-// regression model is y = X * beta + epsilon,  epsilon ~ N(0,sigma2)
-// c0/2 is the prior shape parameter for sigma2
-// d0/2 is the prior scale parameter for sigma2 
-int NormIGregress_sigma2_draw (const gsl_matrix* X, const gsl_vector* Y,
-			       const gsl_vector* beta, double c0, double d0,
-			       gsl_rng* rng, gsl_vector* out) {
-  int k = X->size2;
+int mcmclib_lm_sigma2_draw (gsl_rng* rng, const gsl_matrix* X, const gsl_vector* Y,
+			    const gsl_vector* beta, double c0, double d0,
+			    gsl_vector* out) {
+  const int k = X->size2;
   gsl_vector* e = gsl_vector_alloc(k);
   gsl_vector_memcpy(e, Y);
   gsl_blas_dgemv(CblasNoTrans, -1.0, X, beta, 1.0, e);
   double SSE;
   gsl_blas_ddot(e, e, &SSE);
   gsl_vector_free(e);
-  int n = X->size1;
+  const int n = X->size1;
   const double c_post = (c0 + n) * 0.5;
   const double d_post = (d0 + SSE) * 0.5;
-
   gsl_vector_set(out, 0, 1.0 / gsl_ran_gamma(rng, c_post, d_post));
   return GSL_SUCCESS;
 }
--- src/lm.h
+++ src/lm.h
@@ -21,11 +21,49 @@
 typedef struct {
 } mcmclib_lm;
 
-/** Alloc extra data for an lm model */
-mcmclib_lm* mcmclib_lm_alloc();
+/** Alloc extra data for an lm model
+    @param X design matrix
+ */
+mcmclib_lm* mcmclib_lm_alloc(const gsl_matrix* X);
 
 /** Free extra data for an lm object */
 void mcmclib_lm_free(mcmclib_lm* p);
+
+
+/** Linear regression with Gaussian errors beta draw
+    (Normal  prior).
+
+    Ported from the MCMCpack R package 'NormNormregress_beta_draw' C++ sources
+
+    regression model is y = X * beta + epsilon,  epsilon ~ N(0,sigma2)
+
+    @param rng RNG
+    @param XX X'X product
+    @param XY X'Y product
+    @param b0 prior mean for beta
+    @param B0 prior var/cov matrix for beta
+    @param sigma2 known errors variance
+    @param out sampled value goes here
+    @return GSL_SUCCESS if all goes right
+*/
+int mcmclib_lm_beta_draw(gsl_rng* rng, const gsl_matrix* XX, const gsl_vector* XY,
+			 const gsl_vector* b0, const gsl_matrix* B0,
+			 double sigma2, gsl_vector* out);
+
+/** Linear regression with Gaussian errors sigma2 draw
+    (inverse-Gamma  prior).
+
+    Ported from the MCMCpack R package C++ sources.
+
+    regression model is y = X * beta + epsilon,  epsilon ~ N(0,sigma2)
+
+    @param c0 c0/2 is the prior shape parameter for sigma2
+    @param d0 d0/2 is the prior scale parameter for sigma2
+    @return GSL_SUCCESS if all goes right
+*/
+int mcmclib_lm_sigma2_draw (gsl_rng* rng, const gsl_matrix* X, const gsl_vector* Y,
+			    const gsl_vector* beta, double c0, double d0,
+			    gsl_vector* out);
 
 /**@}*/
 /**@}*/
