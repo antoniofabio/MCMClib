e31f953dbf746d5ba28654764522cf6eb6d24abd
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-16 19:21:12 +0200

    added poisson model with minimal testcase

---
+++ src/pois_model.h
@@ -0,0 +1,43 @@
+/*
+ *  MCMClib: A C Library for doing MCMC
+ *  Copyright (C) 2009 Antonio, Fabio Di Narzo
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ */
+#ifndef __MCMCLIB_POIS_MODEL_H__
+#define __MCMCLIB_POIS_MODEL_H__
+/**\addtogroup Models
+ @{*/
+/**\addtogroup Poisson
+ @{*/
+
+#include <gsl/gsl_matrix.h>
+
+typedef struct {
+  gsl_vector* beta;
+
+  /*internal*/
+  gsl_matrix* X; /**< regression matrix */
+  const gsl_vector* y; /**< observed values */
+  gsl_vector* b0; /**< prior beta mean */
+  gsl_matrix* B0; /**< prior beta precision */
+  gsl_vector* mu; /**< log-means (= X beta) */
+  double ldet; /**< log-determinant of the inverse of B0 */
+  gsl_vector *work1, *work2; /**< workspace memory */
+} mcmclib_pois_model;
+
+mcmclib_pois_model* mcmclib_pois_model_alloc(const gsl_matrix* X, const gsl_vector* y);
+void mcmclib_pois_model_free(mcmclib_pois_model* p);
+int mcmclib_pois_model_set_prior_mean(mcmclib_pois_model* p, const gsl_vector* b0);
+int mcmclib_pois_model_set_prior_var(mcmclib_pois_model* p, const gsl_matrix* B0);
+
+double mcmclib_pois_model_llik(mcmclib_pois_model* p, gsl_vector* x);
+double mcmclib_pois_model_lprior(mcmclib_pois_model* p, gsl_vector* x);
+double mcmclib_pois_model_lpdf(mcmclib_pois_model* p, gsl_vector* x);
+
+/**@}*/
+/**@}*/
+#endif
---
+++ test/tpois_model.c
@@ -0,0 +1,67 @@
+/**Test MCAR model */
+#include <stdio.h>
+#include <assert.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
+#include <pois_model.h>
+
+#define TOL 1e-6
+static int check_dequal(double a, double b) {
+  return (fabs(a-b) < TOL);
+}
+
+#define P 3
+#define N 4
+
+static gsl_vector* x;
+static mcmclib_pois_model* mod;
+
+double llik(double sx) {
+  gsl_vector_set_all(x, sx);
+  return mcmclib_pois_model_llik(mod, x);
+}
+
+double lprior(double sx) {
+  gsl_vector_set_all(x, sx);
+  return mcmclib_pois_model_lprior(mod, x);
+}
+
+double lpdf(double sx) {
+  gsl_vector_set_all(x, sx);
+  return mcmclib_pois_model_lpdf(mod, x);
+}
+
+int main(int argc, char** argv) {
+  gsl_matrix* X = gsl_matrix_alloc(N, P);
+  gsl_matrix_set_zero(X);
+  for(int i=0; i<P; i++)
+    gsl_matrix_set(X, i, i, 1.0);
+  gsl_vector* y = gsl_vector_alloc(N);
+  gsl_vector_set_all(y, 2.0);
+  mod = mcmclib_pois_model_alloc(X, y);
+  gsl_matrix_free(X);
+
+  assert(mod->beta->size == P);
+  x = gsl_vector_alloc(P);
+  llik(0.0);
+  llik(1.0);
+  llik(2.0);
+
+  lprior(1.0);
+  assert(check_dequal(lpdf(1.0), lprior(1.0) + llik(1.0)));
+
+  gsl_vector* b0 = gsl_vector_alloc(P);
+  gsl_vector_set_all(b0, 1.0);
+  mcmclib_pois_model_set_prior_mean(mod, b0);
+  gsl_vector_free(b0);
+  gsl_matrix* B0 = gsl_matrix_alloc(P, P);
+  gsl_matrix_set_identity(B0);
+  mcmclib_pois_model_set_prior_var(mod, B0);
+  gsl_matrix_free(B0);
+
+  mcmclib_pois_model_free(mod);
+  gsl_vector_free(y);
+}
