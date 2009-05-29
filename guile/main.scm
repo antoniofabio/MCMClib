0b3d3d868d1c69c43f4a45a01d76fa4a46a182f2
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-05-29 14:57:22 +0200

    rewritten all guile scheme code loops as eager comprehensions

--- guile/main.scm
+++ guile/main.scm
@@ -2,27 +2,26 @@
 
 (use-modules (srfi srfi-1)
 	     (srfi srfi-42)
-             (swig gsl))
+             (swig gsl)
+             (swig mcmclib))
 
 ;;list -> g-vector
 (define (l2v ll)
   (let*
       ((n (length ll))
        (v (new-gsl-vector n)))
-    (do-ec (: i n) (gsl-vector-set v i (list-ref ll i)))))
+    (do-ec (: i n) (gsl-vector-set v i (list-ref ll i)))
+    v))
 
 ;;g-matrix -> g-vector
 (define (M2v M)
   (let* ((n (gsl-matrix-size1-get M))
          (p (gsl-matrix-size2-get M))
          (v (new-gsl-vector (* n p))))
-    (let loop-i ((i 0))
-      (let loop-j ((j 0))
-        (gsl-vector-set v
+    (do-ec (: i n) (: j p)
+           (gsl-vector-set v
                         (+ (* i p) j)
-                        (gsl-matrix-get M i j))
-        (if (< j (- n 1)) (loop-j (+ j 1))))
-      (if (< i (- p 1)) (loop-i (+ i 1))))
+                        (gsl-matrix-get M i j)))
     v))
 
 ;;list of lists -> matrix
@@ -30,47 +29,30 @@
   (let* ((n (length ll))
          (p (length (car ll)))
          (M (new-gsl-matrix n p)))
-    (let loop-i ((i 0))
-      (let loop-j ((j 0))
-        (let ((row (list-ref ll i)))
-          (gsl-matrix-set M i j (list-ref row j)))
-        (if (< j (- p 1)) (loop-j (+ j 1))))
-      (if (< i (- n 1)) (loop-i (+ i 1))))
+    (do-ec (: i n) (: j p)
+           (gsl-matrix-set M i j (list-ref (list-ref ll i) j)))
     M))
 (ll2M '((1 2 3) (4 5 6)))
 
-(define (dec n) (- n 1))
-
-;;generate a sequence
-(define (seq i)
-  (cond
-   ((= i 1) (list 1))
-   (else
-    (append
-     (seq (- i 1))
-     (list i)))))
-
 ;;g-vector -> list
 (define (v2l v)
-  (map
-   (lambda (i) (gsl-vector-get v i))
-   (map dec (seq (gsl-vector-size-get v)))))
+  (list-ec (: i (gsl-vector-size-get v))
+           (gsl-vector-get v i)))
 (v2l (l2v '(3 1 4)))
 
 ;;get matrix row as a list
 (define (M2l M i)
-  (map
-   (lambda (j) (gsl-matrix-get M i j))
-   (map dec (seq (gsl-matrix-size2-get M)))))
+  (list-ec (: j (gsl-matrix-size2-get M))
+           (gsl-matrix-get M i j)))
 (define M (ll2M '((1 2 3) (4 5 6))))
 (M2l M 0)
 (M2l M 1)
 
 ;;g-matrix -> list of lists
 (define (M2ll M)
-  (map
-   (lambda (i) (M2l M i))
-   (map dec (seq (gsl-matrix-size1-get M)))))
+  (list-ec (: i (gsl-matrix-size1-get M))
+           (list-ec (: j (gsl-matrix-size2-get M))
+                    (gsl-matrix-get M i j))))
 (M2ll (ll2M '((1 2 3) (4 5 6))))
 
 ;;display g-vector
@@ -78,16 +60,17 @@
 ;;display g-Matrix
 (define (dM M) (display (M2ll M)) (newline))
 
+(use-modules (swig mcmclib))
+
 (define P 3)
 (define Psi (new-gsl-matrix P P))
 (gsl-matrix-set-identity Psi)
-(define m P)
-(define p (mcmclib-iwishart-lpdf-alloc Psi m))
+(define p (mcmclib-iwishart-lpdf-alloc Psi P))
 
 (define v (new-gsl-vector (* P P)))
 (gsl-vector-set-all v 1.0)
 (mcmclib-iwishart-lpdf-compute p v)
 
 (define (gsl-copy-subvec dest src offset)
-  (do ((i 0 (1+ i))) ((>= i (gsl-vector-size-get src)))
-    (gsl-vector-set dest (+ offset i) (gsl-vector-get src i))))
+  (do-ec (: i (gsl-vector-size-get src))
+         (gsl-vector-set dest (+ offset i) (gsl-vector-get src i))))
