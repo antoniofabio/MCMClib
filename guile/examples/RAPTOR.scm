54b761913c21d37aad793f358d75f2650f373144
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-07-25 18:13:24 +0200

    cleaned out RAPTOR guile example

--- guile/examples/RAPTOR.scm
+++ guile/examples/RAPTOR.scm
@@ -7,42 +7,22 @@
 
 (define *n* 2) ;;target distribution dimension
 (define *K* 2) ;;number of mixture components
-(define *rng* (new-gsl-rng (gsl-rng-default)))
-(define *x* (new-gsl-vector *n*)) ;;current chain state
 (define *T0* 10000) ;;burn-in length
+(define *N* 100000) ;;number of iterations after burn-in
 
-(define *beta-hat* (new-gsl-vector *K*))
-(define *mu-hat* (new-vectorArray *K*))
-(define *Sigma-hat* (new-matrixArray *K*))
-
+(define *beta-hat* (new-gsl-vector *K*)) ;;starting mixture weigths estimates
 (gsl-vector-set-all *beta-hat* (/ *K*))
-(define (v2gv v)
-  (let*
-      ((n (vector-length v))
-       (gv (new-gsl-vector n)))
-    (do-ec (: i n)
-           (gsl-vector-set gv i (vector-ref v i)))
-    gv))
-(define mu1 (v2gv #(-5 -5)))
-(vectorArray-setitem *mu-hat* 0 mu1)
-(define mu2 (v2gv #(5 5)))
-(vectorArray-setitem *mu-hat* 1 mu2)
 
-(define (ll2M ll)
-  (let* ((n (length ll))
-         (p (length (car ll)))
-         (M (new-gsl-matrix n p)))
-    (do-ec (: i n) (: j p)
-           (gsl-matrix-set M i j (list-ref (list-ref ll i) j)))
-    M))
-(define *Sigma-zero* (ll2M '((1 0) (0 1))))
+(define *Sigma-zero* (new-gsl-matrix *n* *n*)) ;;starting variances estimates
+(gsl-matrix-set-identity *Sigma-zero*)
+(define *Sigma-hat* (new-matrixArray *K*))
 (matrixArray-setitem *Sigma-hat* 0 *Sigma-zero*)
 (matrixArray-setitem *Sigma-hat* 1 *Sigma-zero*)
 
-;;Build a gaussian-mixture target distribution
-;;d: distance between means
-;;S: ratio between variances
 (define (make-target d S)
+  "Build a gaussian-mixture target distribution
+   d: distance between means
+   S: ratio between variances"
   (let
       ((w (new-gsl-vector 2))
        (Sigma1 (new-gsl-matrix *n* *n*))
@@ -67,18 +47,38 @@
     (lambda (x)
       (mcmclib-mixnorm-lpdf-compute pi-mix x))))
 
-(define target (make-target 0 4))
-
-(gsl-vector-set-all *x* 0.5)
-
-;;Make a RAPTOR sampler for distance 'd', ratio 'S'
-(define *sampler* (mcmclib-raptor-alloc *rng*
+(define (simulate-raptor d S)
+  (let
+      (
+       (rng (new-gsl-rng (gsl-rng-default))) ;;random number generator
+       (x (new-gsl-vector *n*)) ;;current chain state
+       (mu-hat (new-vectorArray *K*))
+       (mu-vec (vector (new-gsl-vector *n*) (new-gsl-vector *n*)))
+       (target (make-target d S))
+       (sampler '())
+       (monitor '())
+       )
+    (gsl-vector-set-all x 0.0)
+    (set! monitor (new-mcmclib-monitor x))
+    (gsl-vector-set-all (vector-ref mu-vec 0) (+ (* d -2) -1))
+    (gsl-vector-set-all (vector-ref mu-vec 1) (+ (* d 2) 1))
+    (do-ec (: i *K*)
+           (vectorArray-setitem mu-hat i (vector-ref mu-vec i)))
+    (set! sampler (mcmclib-raptor-alloc rng
                                         (mcmclib-guile-lpdf-cb) (guile-to-voidptr target)
-                                        *x* *T0*
+                                        x *T0*
                                         *Sigma-zero*
                                         *beta-hat*
-                                        *mu-hat*
+                                        mu-hat
                                         *Sigma-hat*))
+    (do-ec (: i *T0*)
+           (mcmclib-amh-update sampler))
+    (do-ec (: i *N*)
+           (begin
+             (mcmclib-amh-update sampler)
+             (mcmclib-monitor-update monitor)))
+    monitor
+  ))
 
 (define (time fun)
   (let*
@@ -87,13 +87,5 @@
        (end (current-time)))
     (- end start)))
 
-(do-ec (: i *T0*)
-       (mcmclib-amh-update *sampler*))
-
-(define mon (new-mcmclib-monitor *x*))
-(time (lambda ()
-        (do-ec (: i 100000)
-               (begin
-                 (mcmclib-amh-update *sampler*)
-                 (mcmclib-monitor-update mon)))))
-(mcmclib-monitor-fprintf-all mon (stdout)) ;;prints means, vars, ARs, MSJDs
+(define *monitor* (simulate-raptor 0 4))
+(mcmclib-monitor-fprintf-all *monitor* (stdout)) ;;prints means, vars, ARs, MSJDs
