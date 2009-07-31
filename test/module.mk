d6d20c33ae2bb3ff950ebce3b179448a5382af84
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-07-31 15:05:27 +0200

    removed scam files, as they are too immature

--- test/module.mk
+++ test/module.mk
@@ -4,7 +4,7 @@ TEST_names:= t1 t2 trecursive_variance tmixem tmixem_rec tmixem_online \
 	tmh_q tmh tmh2 tgauss_rw tgauss_mrw \
 	tamh tamh2 trapt_q trapt traptor tgauss_am \
 	tinca trapt_inca tam_inca tinca_raptor \
-	tspatial tscam tmcar_tilde twishart tmcar_model tiwishart \
+	tspatial tmcar_tilde twishart tmcar_model tiwishart \
 	tgivens tmonitor thier tpois_model tmonitor2
 
 TEST_targets:=$(TEST_names:%=test_%)
