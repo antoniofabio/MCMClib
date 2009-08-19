d076c28ba4ac7ce2a42c215b53c8d9b8ab2d0c63
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-08-19 15:19:22 +0200

    fixed raptor alpha_star fun

--- src/raptor_alpha.c
+++ src/raptor_alpha.c
@@ -21,24 +21,38 @@ double mcmclib_raptor_alpha_star_fun(mcmclib_raptor_gamma* g) {
   int K = g->beta_hat->size;
   int d = g->mu_hat[0]->size;
   gsl_matrix* work = gsl_matrix_alloc(d, d);
-  double within = 0.0;
+  gsl_matrix* W = gsl_matrix_alloc(d, d);
+  gsl_matrix_set_zero(W);
   for(int k=0; k<K; k++) {
     gsl_matrix_memcpy(work, g->Sigma_hat[k]);
-    if(mcmclib_cholesky_decomp(work) == GSL_SUCCESS)
-      within += exp(mcmclib_matrix_logtrace(work));
+    gsl_matrix_scale(work, gsl_vector_get(g->beta_hat, k));
+    gsl_matrix_add(W, work);
   }
-  gsl_vector* mean = gsl_vector_alloc(d);
-  int n=0;
-  for(int k=0; k<K; k++)
-    mcmclib_covariance_update(work, mean, &n, g->mu_hat[k]);
-  double between = 0.0;
-  if(mcmclib_cholesky_decomp(work) == GSL_SUCCESS)
-    between = exp(mcmclib_matrix_logtrace(work));
-  if(between == within)
-    return 0.5;
+  double lwithin = log(0.0);
   gsl_matrix_free(work);
-  gsl_vector_free(mean);
-  return between / (between + within);
+  if(mcmclib_cholesky_decomp(W) == GSL_SUCCESS) {
+    lwithin = mcmclib_matrix_logtrace(W);
+  } else {
+    gsl_matrix_free(W);
+    return 1.0;
+  }
+  gsl_matrix_free(W);
+  gsl_matrix* B = gsl_matrix_alloc(d, d);
+  gsl_matrix_set_zero(B);
+  for(int k=0; k<K; k++) {
+    gsl_matrix_view Mkv = gsl_matrix_view_vector(g->mu_hat[k], d, 1);
+    gsl_matrix* Mk = &(Mkv.matrix);
+    double wk = gsl_vector_get(g->beta_hat, k);
+    gsl_blas_dgemm (CblasNoTrans, CblasTrans, wk * (1.0 - wk),
+		    Mk, Mk, 1.0, B);
+  }
+  double lbetween = 0.0;
+  if(mcmclib_cholesky_decomp(B) == GSL_SUCCESS)
+    lbetween = mcmclib_matrix_logtrace(B);
+  gsl_matrix_free(B);
+  if(lbetween == lwithin)
+    return 0.5;
+  return exp(lbetween) / (exp(lbetween) + exp(lwithin));
 }
 
 double mcmclib_raptor_alpha_identity_fun(void* ignore, mcmclib_raptor_gamma* g) {
