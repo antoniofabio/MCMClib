9a706570c3a2eec3ae76e2754187632327cb44b5
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-09-09 18:03:18 +0200

    fixed mem. leak in rapt testing prog

--- test/trapt.c
+++ test/trapt.c
@@ -101,6 +101,7 @@ int main(int argc, char** argv) {
   }
 
   /*free memory*/
+  gsl_matrix_free(X);
   for(int k=0; k<K; k++)
     gsl_matrix_free(sigma_local[k]);
   gsl_matrix_free(sigma_whole);
