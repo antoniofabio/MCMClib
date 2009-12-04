52ac8e7d21b5ca3adbddb44c87899308f0ffc1b3
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-12-04 12:48:17 +0100

    updated examples

--- guile/examples/empiricalCDF.scm
+++ guile/examples/empiricalCDF.scm
@@ -1,24 +1,12 @@
 ;;
 ;;Compute the empirical CDF 'live' on a running chain
 ;;
-(set! %load-path (cons "." %load-path))
-
 (use-modules (srfi srfi-1)
 	     (srfi srfi-42)
              (swig gsl)
              (swig mcmclib)
+             (swig mcmcutils)
              (ice-9 format))
-
-(define (gv2v gv)
-  (vector-ec (: i (gsl-vector-size-get gv)) (gsl-vector-get gv i)))
-(define (v2gv v)
-  (let*
-      ((n (vector-length v))
-       (gv (new-gsl-vector n)))
-    (do-ec (: i n)
-           (gsl-vector-set gv i (vector-ref v i)))
-    gv))
-
 
 (define *N-X0* 1000)
 (define *dim* 5)
