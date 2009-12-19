22e00915b98be510ab90428d69bfb4fb075d46b7
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-12-19 17:02:52 +0100

    polish amh class

--- src/amh.c
+++ src/amh.c
@@ -10,12 +10,14 @@
 #include "amh.h"
 
 mcmclib_amh* mcmclib_amh_alloc(mcmclib_mh* mh, void* suff,
-			       mcmclib_amh_update_gamma_p update_gamma) {
+			       mcmclib_amh_update_gamma_p update_gamma,
+			       free_fun_t free_suff) {
   mcmclib_amh* p = (mcmclib_amh*) malloc(sizeof(mcmclib_amh));
   p->mh = mh;
   p->suff = suff;
   p->update_gamma = update_gamma;
   p->n = 0;
+  p->free_suff = free_suff;
   return p;
 }
 
@@ -25,6 +27,8 @@ void mcmclib_amh_reset(mcmclib_amh* p) {
 }
 
 void mcmclib_amh_free(mcmclib_amh* p) {
+  if(p->free_suff)
+    p->free_suff(p->suff);
   free(p);
 }
 
