3ad0ba8a91c580fd273921f56962b934f6059e1f
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-12-19 16:50:39 +0100

    polish mh-q class

--- src/mh_q.c
+++ src/mh_q.c
@@ -10,29 +10,30 @@
 #include "mh_q.h"
 
 mcmclib_mh_q* mcmclib_mh_q_alloc(gsl_rng* r,
-				 samplerfun_p sampler_fun, void* sampler_data,
-				 proposal_distr_t qd_fun, void* qd_data,
-				 void* gamma) {
+				 sampler_fun_t rq,
+				 proposal_distr_fun_t dq,
+				 void* gamma, free_fun_t free_gamma_fun) {
   mcmclib_mh_q* a = (mcmclib_mh_q*) malloc(sizeof(mcmclib_mh_q));
   a->r = r;
-  a->sampler_fun = sampler_fun;
-  a->sampler_data = sampler_data;
-  a->qd_fun = qd_fun;
-  a->qd_data = qd_data;
+  a->rq = rq;
+  a->dq = dq;
   a->gamma = gamma;
+  a->free_gamma_fun = free_gamma_fun;
   return a;
 }
 
 void mcmclib_mh_q_free(mcmclib_mh_q* p) {
+  if(p->free_gamma_fun)
+    p->free_gamma_fun(p->gamma);
   free(p);
 }
 
 void mcmclib_mh_q_sample(mcmclib_mh_q* p, gsl_vector* x) {
-  p->sampler_fun(p, x);
+  p->rq(p, x);
 }
 
 double mcmclib_mh_q_logd(mcmclib_mh_q* p, gsl_vector* x, gsl_vector* y) {
-  return p->qd_fun(p->qd_data, x, y);
+  return p->dq(p, x, y);
 }
 
 double mcmclib_mh_q_ratio_offset(mcmclib_mh_q* p, gsl_vector* x, gsl_vector* y) {
