2aafb9113922c1e5d2fff17baa0fd3e02e695351
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-12-19 13:58:42 +0100

    clean makefile

--- guile/module.mk
+++ guile/module.mk
@@ -1,21 +1,21 @@
 GUILE_MODULES := mcmclib
-GUILE_MODULES_LIBS := $(GUILE_MODULES:%=guile/swig/libguile%.so)
-GUILE_MODULES_TARGETS := $(GUILE_MOULES_LIBS) $(GUILE_MODULES:%=guile/%_wrap.c)
+GUILE_MODULES_LIBS := $(GUILE_MODULES:%=guile/libguile%.so)
+SRC += $(GUILE_MODULES:%=guile/%_wrap.c) $(GUILE_MODULES:%=guile/swig/%.scm)
 GUILE_LIBRARY_PATH:= $(shell guile -c "(display (%package-data-dir))")
 
 SWIG_FLAGS := -guile -scm -nodefaultctor -package swig -Linkage passive -scmstub
 GUILE_LDFLAGS := $(LDFLAGS) -lguile
 GUILE_CFLAGS := $(CFLAGS) -I./src
 
-TOCLEAN += $(GUILE_MODULES:%=guile/swig/libguile%.so)
+TOCLEAN += $(GUILE_MODULES_LIBS)
 
-guile: $(GUILE_MODULES_TARGETS)
+guile: $(GUILE_MODULES_LIBS)
 
 guile/mcmclib_wrap.c: guile/monitor.i guile/amh.i guile/distrfuns.i guile/mixem.i guile/at7.i
-guile/%_wrap.c: guile/%.i
+guile/%_wrap.c guile/swig/%.scm: guile/%.i
 	swig $(SWIG_FLAGS) -o $@ $<
 
-guile/swig/libguile%.so: guile/%_wrap.c src/libmcmclib.a
+guile/libguile%.so: guile/%_wrap.c src/libmcmclib.a
 	$(CC) -shared $(GUILE_CFLAGS) -fPIC $^ $(GUILE_LDFLAGS) -o $@
 
 install-guile: guile
