230758b996682d54c227dd01bf183914880d43dd
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-12-20 19:01:35 +0100

    add rapt testing script

--- test-guile/module.mk
+++ test-guile/module.mk
@@ -4,4 +4,4 @@ SRC += $(wildcard test-guile/*.scm)
 
 test-guile:
 	GUILE_LOAD_PATH=. ./test-guile/guile-test --test-suite test-guile\
-		mh_q.test mh.test amh.test mcmcutils.test
+		mh_q.test mh.test amh.test mcmcutils.test rapt.test
