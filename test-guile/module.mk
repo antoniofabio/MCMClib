bd1f127e235cc312f96704ba9338b82796eb7d29
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-12-20 15:33:52 +0100

    restrict running tests

--- test-guile/module.mk
+++ test-guile/module.mk
@@ -3,4 +3,5 @@ SRC += $(wildcard test-guile/*.dat)
 SRC += $(wildcard test-guile/*.scm)
 
 test-guile:
-	GUILE_LOAD_PATH=. ./test-guile/guile-test --test-suite test-guile
+	GUILE_LOAD_PATH=. ./test-guile/guile-test --test-suite test-guile\
+		mh_q.test mh.test amh.test
