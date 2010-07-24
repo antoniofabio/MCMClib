21543014e418131b7c17e083308f9169f6a254f1
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-07-24 11:38:10 +0200

    update at7 to the new amh api

--- src/at7.c
+++ src/at7.c
@@ -12,10 +12,32 @@
 #include "mvnorm.h"
 #include "matrix.h"
 
-#define AT7_GAMMA(p) ((mcmclib_at7_gamma*)((mcmclib_amh*) p)->mh->q->gamma)
-#define AT7_SUFF(p) ((mcmclib_at7_suff*)((mcmclib_amh*) p)->suff)
+/** \brief AT7 gamma values */
+typedef struct {
+  gsl_vector* beta; /**< current mixture weights*/
+  gsl_vector** mu; /**< current mixture means*/
+  gsl_matrix** Sigma; /**< current mixture variances*/
 
-void at7_gamma_update_Sigma(mcmclib_at7_gamma* p) {
+  mcmclib_mvnorm_lpdf** pik; /**< single mixture components densities*/
+  mcmclib_mixnorm_lpdf* pi; /**< fitted mixture density*/
+
+  gsl_matrix** qVariances; /**< local proposal variances*/
+  mcmclib_mvnorm_lpdf** qdk; /**< proposal density components*/
+
+  gsl_matrix* Sigma_eps; /**< positive-definiteness correction factor*/
+  gsl_vector* scaling_factors; /**< region-specific scaling factors*/
+
+  gsl_vector* tmpMean; /**< internal workspace memory*/
+  gsl_vector* weights; /**< internal workspace memory*/
+} at7_gamma;
+
+/** \brief AT7 sufficient data */
+typedef mcmclib_mixem_online* at7_suff;
+
+#define AT7_GAMMA(p) ((at7_gamma*)((mcmclib_amh*) p)->mh->q->gamma)
+#define AT7_SUFF(p) ((at7_suff*)((mcmclib_amh*) p)->suff)
+
+static void at7_gamma_update_Sigma(at7_gamma* p) {
   const size_t K = p->beta->size;
   for(size_t k=0; k < K; k++) {
     mcmclib_matrix_addscale(p->qVariances[k],
@@ -24,10 +46,11 @@ void at7_gamma_update_Sigma(mcmclib_at7_gamma* p) {
   }
 }
 
-mcmclib_at7_gamma* mcmclib_at7_gamma_alloc(const gsl_vector* beta,
-					   gsl_vector** mu,
-					   gsl_matrix** Sigma) {
-  mcmclib_at7_gamma* ans = (mcmclib_at7_gamma*) malloc(sizeof(mcmclib_at7_gamma));
+/** alloc a new at7_gamma object. Input arguments are copied @internal */
+static at7_gamma* at7_gamma_alloc(const gsl_vector* beta,
+				  gsl_vector** mu,
+				  gsl_matrix** Sigma) {
+  at7_gamma* ans = (at7_gamma*) malloc(sizeof(at7_gamma));
   const size_t K = beta->size;
   const size_t dim = mu[0]->size;
   ans->beta = gsl_vector_alloc(K);
@@ -60,9 +83,10 @@ mcmclib_at7_gamma* mcmclib_at7_gamma_alloc(const gsl_vector* beta,
   return ans;
 }
 
-void mcmclib_at7_gamma_free(void* in_p) {
+/** frees an at7_gamma object @internal*/
+static void at7_gamma_free(void* in_p) {
   if(!in_p) return;
-  mcmclib_at7_gamma* p = (mcmclib_at7_gamma*) in_p;
+  at7_gamma* p = (at7_gamma*) in_p;
   for(size_t k=0; k < p->beta->size; k++) {
     mcmclib_mvnorm_lpdf_free(p->pik[k]);
     gsl_vector_free(p->mu[k]);
@@ -84,7 +108,7 @@ void mcmclib_at7_gamma_free(void* in_p) {
   free(p);
 }
 
-static void at7_components_weights(mcmclib_at7_gamma* g, const gsl_vector* x) {
+static void at7_components_weights(at7_gamma* g, const gsl_vector* x) {
   mcmclib_mvnorm_lpdf** pik = g->pik;
   double sum=0.0;
   for(size_t k=0; k < g->weights->size; k++) {
@@ -108,7 +132,7 @@ static size_t sample(gsl_rng* r, const gsl_vector* probs) {
 }
 
 static void at7_q_sample(mcmclib_mh_q* q, gsl_vector* x) {
-  mcmclib_at7_gamma* gamma = (mcmclib_at7_gamma*) q->gamma;
+  at7_gamma* gamma = (at7_gamma*) q->gamma;
   at7_components_weights(gamma, x);
   const size_t k = sample(q->r, gamma->weights);
   gsl_vector* oldx = gsl_vector_alloc(x->size);
@@ -119,7 +143,7 @@ static void at7_q_sample(mcmclib_mh_q* q, gsl_vector* x) {
 }
 
 static double at7_q_d(mcmclib_mh_q* q, gsl_vector* x, gsl_vector* y) {
-  mcmclib_at7_gamma* gamma = (mcmclib_at7_gamma*) q->gamma;
+  at7_gamma* gamma = (at7_gamma*) q->gamma;
   at7_components_weights(gamma, x);
   gsl_vector_memcpy(gamma->tmpMean, x);
   const size_t K = gamma->beta->size;
@@ -131,12 +155,29 @@ static double at7_q_d(mcmclib_mh_q* q, gsl_vector* x, gsl_vector* y) {
   return log(ans);
 }
 
-mcmclib_at7_suff* mcmclib_at7_suff_alloc(mcmclib_at7_gamma* g, size_t t0) {
-  return (mcmclib_at7_suff*) mcmclib_mixem_online_alloc(g->mu, g->Sigma, g->beta, 0.5, t0);
+/** alloc a new AT7 sampler suff stats object
+@param t0 burn-in length before starting adaptation
+@returns a new AT7_suff object
+*/
+at7_suff* at7_suff_alloc(at7_gamma* g, size_t t0) {
+  return (at7_suff*) mcmclib_mixem_online_alloc(g->mu, g->Sigma, g->beta, 0.5, t0);
 }
 
-void mcmclib_at7_suff_free(void* p) {
+/** free raptor_suff data*/
+static void at7_suff_free(void* p) {
   mcmclib_mixem_online_free((mcmclib_mixem_online*) p);
+}
+
+/** Update suff stats of an AT7 chain*/
+static void at7_suff_update(void* suff, const gsl_vector* x) {
+  mcmclib_mixem_online* em = (mcmclib_mixem_online*) suff;
+  mcmclib_mixem_online_update(em, x);
+}
+
+static void at7_gamma_update(void* suff, void* gamma) {
+  suff = NULL; /*keep compiler quiet*/
+  /*'suff' contains references to 'gamma' stuff*/
+  at7_gamma_update_Sigma(gamma);
 }
 
 mcmclib_amh* mcmclib_at7_alloc(gsl_rng* r,
@@ -145,19 +186,21 @@ mcmclib_amh* mcmclib_at7_alloc(gsl_rng* r,
 			       const gsl_vector* beta_hat,
 			       gsl_vector** mu_hat,
 			       gsl_matrix** Sigma_hat){
-  mcmclib_at7_gamma* gamma = mcmclib_at7_gamma_alloc(beta_hat,
-						     mu_hat,
-						     Sigma_hat);
+  at7_gamma* gamma = at7_gamma_alloc(beta_hat,
+				     mu_hat,
+				     Sigma_hat);
   mcmclib_mh_q* q = mcmclib_mh_q_alloc(r, at7_q_sample, at7_q_d,
-				       gamma, mcmclib_at7_gamma_free);
+				       gamma, at7_gamma_free);
   mcmclib_mh* mh = mcmclib_mh_alloc(r, logdistr, logdistr_data, q, x);
-  mcmclib_at7_suff* suff = mcmclib_at7_suff_alloc(gamma, t0);
-  mcmclib_amh* ans = mcmclib_amh_alloc(mh, suff, mcmclib_at7_update,
-				       mcmclib_at7_suff_free);
+  at7_suff* suff = at7_suff_alloc(gamma, t0);
+  mcmclib_amh* ans = mcmclib_amh_alloc(mh, t0, suff,
+				       at7_suff_free,
+				       at7_suff_update,
+				       at7_gamma_update);
   return ans;
 }
 
-void mcmclib_at7_set_sf_all(mcmclib_amh* p, double sf) {
+void mcmclib_at7_set_sf_all(mcmclib_amh* p, const double sf) {
   gsl_vector_set_all(AT7_GAMMA(p)->scaling_factors, sf);
 }
 
@@ -172,12 +215,4 @@ void mcmclib_at7_set_sf(mcmclib_amh* p, const gsl_vector* sf) {
 	  gsl_vector_get(AT7_GAMMA(p)->mu[0], 0),			\
 	  gsl_vector_get(AT7_GAMMA(p)->mu[1], 0),			\
 	  gsl_matrix_get(AT7_GAMMA(p)->Sigma[0], 0, 0),			\
-	  gsl_matrix_get(AT7_GAMMA(p)->Sigma[1], 0, 0))			\
-
-void mcmclib_at7_update(mcmclib_amh* p) {
-  mcmclib_mixem_online* em = (mcmclib_mixem_online*) AT7_SUFF(p);
-  mcmclib_mixem_online_update(em, p->mh->x);
-  if((p->n) > em->n0) {
-    at7_gamma_update_Sigma(AT7_GAMMA(p));
-  }
-}
+	  gsl_matrix_get(AT7_GAMMA(p)->Sigma[1], 0, 0))
