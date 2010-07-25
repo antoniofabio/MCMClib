c381790d6ed231184875bdd0b17a4ec326f49e9a
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-07-25 10:05:19 +0200

    start tests refactoring: amh

--- test/tamh.c
+++ test/tamh.c
@@ -6,6 +6,7 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <amh.h>
+#include "CuTest.h"
 
 #define v0(x) gsl_vector_get(x, 0)
 #define x0 v0(x)
@@ -24,28 +25,26 @@ static void sampler(mcmclib_mh_q* p, gsl_vector* x) {
   gsl_vector_set(x, 0, x0 + (*o));
 }
 
-static void update_gamma(mcmclib_amh* p) {
-  int *suff = (int*) p->suff;
-  (*suff)++;
+static void update_suff(void* p, const gsl_vector* x) {
+  int *suff = (int*) p;
+  suff[0]++;
 }
 
-int main() {
+void Testamh(CuTest *tc) {
   gsl_rng* r = gsl_rng_alloc(gsl_rng_default);
   gsl_vector* x = gsl_vector_alloc(1);
   gsl_vector_set(x, 0, 0.5);
   double inc = 0.2;
-  int gamma = 0;
+  int suff = 0;
   mcmclib_amh* s = mcmclib_amh_alloc(mcmclib_mh_alloc(r, dtarget, NULL,
-						      mcmclib_mh_q_alloc(r, sampler, NULL, &inc, NULL), x),
-				     &gamma, update_gamma, NULL);
+						      mcmclib_mh_q_alloc(r, sampler, NULL, &inc, NULL), x), 0,
+				     &suff, NULL, update_suff, NULL);
   for(int n=0; n<10; n++) {
     mcmclib_amh_update(s);
   }
-  assert(gamma == 10);
+  CuAssertIntEquals(tc, 10, suff);
 
   gsl_vector_free(x);
   gsl_rng_free(r);
   mcmclib_amh_free(s);
-
-  return 0;
 }
