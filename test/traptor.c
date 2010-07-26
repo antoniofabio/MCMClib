94eab765d253431cd0730f2bb204c10a783011fb
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-07-26 22:11:50 +0200

    refactor raptor test

--- test/traptor.c
+++ test/traptor.c
@@ -6,7 +6,8 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <region_mixnorm.h>
-#include <raptor.h>
+#include "raptor.c"
+#include "CuTest.h"
 
 static const double beta = 0.5;
 static const double V[] = {1.0, 1.0};
@@ -23,9 +24,6 @@ static const double MU[] = {0.2, 0.8};
 #define m00(m) gsl_matrix_get(m, 0, 0)
 
 #define TOL 1e-6
-int check_dequal(double a, double b) {
-  return (fabs(a-b) < TOL);
-}
 
 static double dunif(void* ignore, const gsl_vector* x) {
   ignore = NULL; /*keep compiler quiet*/
@@ -38,7 +36,7 @@ double fix(double in) {
   return (in + 0.001) * SF;
 }
 
-int main() {
+void Testraptor(CuTest* tc) {
   gsl_vector* x = gsl_vector_alloc(DIM);
   gsl_vector_set_all(x, 0.0);
 
@@ -77,18 +75,18 @@ int main() {
   }
 
   mcmclib_rapt_gamma* q = (mcmclib_rapt_gamma*) sampler->mh->q->gamma;
-  mcmclib_raptor_gamma* g = (mcmclib_raptor_gamma*) q->which_region_data;
-  assert(check_dequal(v0(g->beta_hat), v0(olem->beta)));
-  assert(check_dequal(v0(g->mu_hat[0]), v0(olem->mu[0])));
-  assert(check_dequal(m00(g->Sigma_hat[0]), m00(olem->Sigma[0])));
+  raptor_gamma* g = (raptor_gamma*) q->which_region_data;
+  CuAssertDblEquals(tc, v0(olem->beta), v0(g->beta_hat), TOL);
+  CuAssertDblEquals(tc, v0(olem->mu[0]), v0(g->mu_hat[0]), TOL);
+  CuAssertDblEquals(tc, m00(olem->Sigma[0]), m00(g->Sigma_hat[0]), TOL);
 
   /*check boundary function*/
   gsl_vector_set_all(x, -1.0);
   size_t rx = mcmclib_region_mixnorm_compute(x, g->pi_hat);
-  assert(rx == 0);
+  CuAssertIntEquals(tc, 0, rx);
   gsl_vector_set_all(x, 1.0);
   rx = mcmclib_region_mixnorm_compute(x, g->pi_hat);
-  assert(rx == 1);
+  CuAssertIntEquals(tc, 1, rx);
 
   /*check options setting*/
   mcmclib_raptor_set_alpha(sampler, 0.2);
@@ -112,6 +110,4 @@ int main() {
   }
   gsl_vector_free(w_hat);
   gsl_vector_free(beta_v);
-
-  return 0;
 }
