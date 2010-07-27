78bdbecb4215d0c097c22502b8c8f8b9724c68f7
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-07-27 10:18:55 +0200

    refactor mh_q and mh tests

--- test/tmh_q.c
+++ test/tmh_q.c
@@ -6,6 +6,7 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <mh_q.h>
+#include "CuTest.h"
 
 #define v0(x) gsl_vector_get(x, 0)
 #define x0 v0(x)
@@ -20,7 +21,7 @@ static void sampler(mcmclib_mh_q* q, gsl_vector* x) {
   gsl_vector_set(x, 0, x0 + (*o));
 }
 
-int main() {
+void Testmh_q(CuTest* tc) {
   gsl_rng* r = gsl_rng_alloc(gsl_rng_default);
   gsl_vector* x = gsl_vector_alloc(1);
   gsl_vector_set(x, 0, 2.0);
@@ -30,12 +31,10 @@ int main() {
   mcmclib_mh_q* q = mcmclib_mh_q_alloc(r, sampler, qd, &inc, NULL);
 
   mcmclib_mh_q_sample(q, x);
-  assert(x0 == 3.0);
+  CuAssertTrue(tc, x0 == 3.0);
 
   mcmclib_mh_q_free(q);
   gsl_vector_free(x);
   gsl_vector_free(y);
   gsl_rng_free(r);
-
-  return 0;
 }
