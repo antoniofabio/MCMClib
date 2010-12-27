ea625f1c68b7f111f035b164803071f609f4dc3a
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-12-27 16:38:56 +0100

    more R-gsl [vector,matrix] utility funcs, update test script

--- R-rawgsl/rawgsl.R
+++ R-rawgsl/rawgsl.R
@@ -21,17 +21,68 @@ dyn.import(pst("gsl_",
                  pst("ran_", pst(distribs, c("", "_pdf"))))),
            rawgsl)
 
-## utility functions
+## utility functions and additional types
 with(rawgsl, {
+
+  gvec.type <- structType(list(size = uint32Type, stride = uint32Type,
+                               data = pointerType, block = pointerType,
+                               owner = sint32Type))
+
+  gvec_size <- function(x) {
+    getStructField(x, "size", gvec.type)
+  }
+  
   gvec_get <- function(x, ii) sapply(as.list(ii), function(i) gsl_vector_get(x, as.integer(i), RETURN=doubleType))
+
+  gvec2vec <- function(x) gvec_get(x, seq_len(gvec_size(x))-1)
+
   gvec_set <- function(x, ii, val) {
     mapply(function(i, val.i) gsl_vector_set(x, as.integer(i), as.double(val.i)),
            as.list(ii), as.list(val))
     return(invisible(val))
   }
+
+  vec2gvec <- function(x) {
+    y <- gsl_vector_alloc(as.integer(length(x)), RETURN = pointerType)
+    gvec_set(y, seq_len(length(x))-1, x)
+    return(y)
+  }
+  
+  gmat.type <- structType(list(size1 = uint32Type, size2 = uint32Type,
+                               tda = uint32Type,
+                               data = pointerType, block = pointerType,
+                               owner = sint32Type))
+
+  gmat_size1 <- function(x) {
+    getStructField(x, "size1", gmat.type)
+  }
+  gmat_size2 <- function(x) {
+    getStructField(x, "size2", gmat.type)
+  }
+  gmat_dim <- function(x) {
+    c(gmat_size1(x), gmat_size2(x))
+  }
+
   gmat_get <- function(x, ii, jj) {
     outer(ii, jj, function(i, j) {
       gsl_matrix_get(x, as.integer(i), as.integer(j), RETURN=doubleType)
     })
   }
+
+  gmat2mat <- function(x) {
+    gmat_get(x, seq_len(gmat_size1(x))-1, seq_len(gmat_size2(x))-1)
+  }
+
+  gmat_set <- function(x, i, j, value) {
+    gsl_matrix_set(x, as.integer(i), as.integer(j), as.double(value))
+  }
+  
+  mat2gmat <- function(x) {
+    y <- gsl_matrix_alloc(0.0, NROW(x), NCOL(x), RETURN = pointerType)
+    for(i in seq_len(NROW(x))) for(j in seq_len(NCOL(x))) {
+      gmat_set(y, i-1, j-1, x[i, j])
+    }
+    return(y)
+  }
+  
 })
--- R/test.R
+++ R/test.R
@@ -4,10 +4,11 @@ attach(mcmclib)
 
 dyn.load("/usr/lib/libgslcblas.so", local=FALSE)
 dyn.load("/usr/lib/libgsl.so", local=FALSE)
-x <- gsl_vector_alloc(23L, RETURN=pointerType)
-gsl_vector_set(x, 7L, -2)
-gsl_vector_get(x, 7L, RETURN=doubleType)
-gsl_vector_free(x)
+x <- rnorm(23)
+gx <- vec2gvec(x)
+x1 <- gvec2vec(gx)
+stopifnot(identical(x, x1))
+gsl_vector_free(gx)
 
 pp <- getNativeValue(getNativeSymbolInfo("gsl_rng_default")$address,
                      pointerType)
@@ -16,16 +17,15 @@ rng <- gsl_rng_alloc(pp, RETURN=pointerType)
 xx <- replicate(1000, gsl_ran_gaussian(rng, 1.0, RETURN=doubleType))
 
 dyn.load("../src/libmcmclib.so", local=FALSE)
-dim <- 1000L
-x <- gsl_vector_alloc(dim, RETURN=pointerType)
-gvec_set(x, seq_len(dim)-1, 0.0)
+dim <- 1000
+x <- vec2gvec(rep(0.0, dim))
 
 dyn.load("f.so")
 f <- getNativeSymbolInfo("f")$address
 sampler <- mcmclib_gauss_scalar_am_alloc(rng, f, 0L, x, 0.1, 1000L,
                                          RETURN = pointerType)
-system.time(mcmclib_amh_update_N(sampler, 100000L))
-uh <- gvec_get(x, seq_len(dim)-1)
+system.time(mcmclib_amh_update_N(sampler, 10000L))
+y <- gvec2vec(x)
 mcmclib_amh_free(sampler)
 
 pp <- getNativeValue(getNativeSymbolInfo("gsl_rng_default")$address,
@@ -34,5 +34,5 @@ rng <- gsl_rng_alloc(pp, RETURN=pointerType)
 gvec_set(x, seq_len(dim)-1, 0.0)
 sampler <- mcmclib_gauss_rw_alloc(rng, f, 0L, x, 0.1,
                                   RETURN = pointerType)
-system.time(mcmclib_mh_update_N(sampler, 100000L))
-uh <- gvec_get(x, seq_len(dim)-1)
+system.time(mcmclib_mh_update_N(sampler, 10000L))
+y <- gvec_get(x, seq_len(dim)-1)
