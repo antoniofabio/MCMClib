93f7fbc7469a942793b987e4f686b037dea1f1a3
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-12-27 17:27:09 +0100

    add monitor_acf R wrappers

--- R/mcmclib.R
+++ R/mcmclib.R
@@ -8,7 +8,9 @@ dyn.import(pst("mcmclib_",
                  "gauss_scalar_am_alloc", "gauss_am_alloc",
                  pst("monitor_", c("alloc", "free", "update")),
                  pst("monitor_get_", c("means", "vars", "ar", "msjd")),
-                 pst("monitor_ecdf_", c("alloc", "free", "update")))),
+                 pst("monitor_ecdf_", c("alloc", "free", "update")),
+                 pst("monitor_acf_", c("alloc", "free", "update", "get")),
+                 "iact_from_acf")),
            mcmclib)
 
 with(mcmclib, {
