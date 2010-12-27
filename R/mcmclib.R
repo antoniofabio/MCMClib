55e3cd826703f8692b0bcdf320801e5de8c31830
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-12-27 15:35:18 +0100

    add '*_update_N' R bindings

--- R/mcmclib.R
+++ R/mcmclib.R
@@ -2,8 +2,8 @@ source("rawgsl.R")
 
 mcmclib <- new.env(parent=rawgsl)
 dyn.import(pst("mcmclib_",
-               c(pst("mh_", c("alloc", "free", "update")),
+               c(pst("mh_", c("alloc", "free", "update", "update_N")),
                  "gauss_rw_alloc", "gauss_mrw_alloc",
-                 pst("amh_", c("alloc", "free", "update")),
+                 pst("amh_", c("alloc", "free", "update", "update_N")),
                  "gauss_scalar_am_alloc", "gauss_am_alloc")),
            mcmclib)
