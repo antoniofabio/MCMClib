48792c3185febd9ef1b3a6d808a5b3bf9e5e82b6
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-12-27 15:53:21 +0100

    update example

--- R/test.R
+++ R/test.R
@@ -16,13 +16,23 @@ rng <- gsl_rng_alloc(pp, RETURN=pointerType)
 xx <- replicate(1000, gsl_ran_gaussian(rng, 1.0, RETURN=doubleType))
 
 dyn.load("../src/libmcmclib.so", local=FALSE)
-x <- gsl_vector_alloc(100L, RETURN=pointerType)
-gvec_set(x, 0:99, 0.0)
+dim <- 1000L
+x <- gsl_vector_alloc(dim, RETURN=pointerType)
+gvec_set(x, seq_len(dim)-1, 0.0)
 
 dyn.load("f.so")
 f <- getNativeSymbolInfo("f")$address
-sampler <- mcmclib_gauss_scalar_am_alloc(rng, f, 0L, x, 3.0, 1000L,
+sampler <- mcmclib_gauss_scalar_am_alloc(rng, f, 0L, x, 0.1, 1000L,
                                          RETURN = pointerType)
-for(i in seq_len(2e4)) mcmclib_amh_update(sampler)
-uh <- Vectorize(function(i) gsl_vector_get(x, as.integer(i), RETURN=doubleType))(seq.int(0, 99))
+system.time(mcmclib_amh_update_N(sampler, 100000L))
+uh <- gvec_get(x, seq_len(dim)-1)
 mcmclib_amh_free(sampler)
+
+pp <- getNativeValue(getNativeSymbolInfo("gsl_rng_default")$address,
+                     pointerType)
+rng <- gsl_rng_alloc(pp, RETURN=pointerType)
+gvec_set(x, seq_len(dim)-1, 0.0)
+sampler <- mcmclib_gauss_rw_alloc(rng, f, 0L, x, 0.1,
+                                  RETURN = pointerType)
+system.time(mcmclib_mh_update_N(sampler, 100000L))
+uh <- gvec_get(x, seq_len(dim)-1)
