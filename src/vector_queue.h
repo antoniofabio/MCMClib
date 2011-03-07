5d1b96b21389aa0fbc841ea90d6e8c16207ca8bb
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2011-03-07 15:11:48 +0100

    write down some minimal vector_queue documentation

--- src/vector_queue.h
+++ src/vector_queue.h
@@ -12,19 +12,28 @@
 
 /**\addtogroup utils
 @{
+\defgroup vector_queue gsl_vector queue
+@{
 */
 
 #include <gsl/gsl_vector.h>
 
 typedef struct mcmclib_vector_queue_t mcmclib_vector_queue;
 
+/**alloc a new vector_queue object*/
 mcmclib_vector_queue* mcmclib_vector_queue_alloc(const size_t dim, const size_t max_size);
+/**free a previously allocated vector_queue object*/
 void mcmclib_vector_queue_free(mcmclib_vector_queue* q);
+/**append a new vector to the queue*/
 int mcmclib_vector_queue_append(mcmclib_vector_queue* q, const gsl_vector* x);
+/**current vector queue size*/
 size_t mcmclib_vector_queue_size(const mcmclib_vector_queue* q);
+/**max vector queue size*/
 size_t mcmclib_vector_queue_size_max(const mcmclib_vector_queue* q);
+/**max vector queue size*/
 int mcmclib_vector_queue_get(const mcmclib_vector_queue* q, const size_t lag, gsl_vector* x);
 
+/**@}*/
 /**@}*/
 
 #endif
